25d31ddd62763821849b8418e70b74ad148ed90e
Cola <bcad1591@gmail.com>
2023-05-05 22:16:22 +0900

    chore: config storybook-test
    - a11y
    - test-runner
    - coverage
    - jest

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,8 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-coverage',
   ],
   framework: {
     name: '@storybook/nextjs',
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { expect } from '@storybook/jest';
+import { within, userEvent } from '@storybook/testing-library';
+
 import { Button } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
@@ -19,6 +22,33 @@ type Story = StoryObj<typeof Button>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    await step('클릭', async () => {
+      await userEvent.click(
+        canvas.getByRole('button', {
+          name: 'Button',
+        })
+      );
+    });
+
+    await step('더블클릭', async () => {
+      await userEvent.dblClick(
+        canvas.getByRole('button', {
+          name: 'Button',
+        })
+      );
+    });
+
+    await step('assertion', () => {
+      expect(
+        canvas.queryByRole('button', {
+          name: 'Not Exists Button',
+        })
+      ).not.toBeInTheDocument();
+    });
+  },
   args: {
     primary: true,
     label: 'Button',
