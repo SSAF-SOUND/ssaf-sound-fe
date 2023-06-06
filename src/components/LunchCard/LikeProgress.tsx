17ed6ebeeaf42e2fd283adbc0bd7c75b413f8bc4
otterp012 <otterp012@gmail.com>
2023-06-06 20:35:07 +0900

    feat: LikeProgress 컴포넌트 추가

---
+++ src/components/LunchCard/LikeProgress.tsx
@@ -0,0 +1,35 @@
+import type { ProgressBarProps } from '../Common/ProgressBar';
+
+import { css } from '@emotion/react';
+
+import ProgressBar from '../Common/ProgressBar';
+
+interface Props extends ProgressBarProps {}
+const LikeProgress = (props: Props) => {
+  const { now } = props;
+  return (
+    <div css={baseCss}>
+      <span css={textCss}>{now}명이 좋아합니다.</span>
+      <ProgressBar
+        now={now}
+        backgroundColor="#F9F9F9"
+        foregroundColor="#9ADFA1"
+      />
+    </div>
+  );
+};
+
+const baseCss = css({
+  margin: '25px 18px 0',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '6px',
+});
+
+const textCss = css({
+  fontSize: '14px',
+  lineHeight: '20px',
+  color: '#fff',
+});
+
+export default LikeProgress;
