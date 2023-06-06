35af2ea1ec9305aa857fa8efa9e7bbde07c8c446
otterp012 <otterp012@gmail.com>
2023-06-06 20:32:43 +0900

    style: 글로벌 css에, box-sizing: border-box 추가

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -95,6 +95,7 @@ const resetCss = css`
     font-size: 100%;
     font: inherit;
     vertical-align: baseline;
+    box-sizing: border-box;
   }
   /* HTML5 display-role reset for older browsers */
   article,
