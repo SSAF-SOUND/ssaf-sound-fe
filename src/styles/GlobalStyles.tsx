dadae331a7bd63181985d69c74dff1d889fc3783
otterp012 <otterp012@gmail.com>
2023-06-07 01:21:21 +0900

    fix: 글로벌 스타일에 box-sizing 삭제

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -95,7 +95,6 @@ const resetCss = css`
     font-size: 100%;
     font: inherit;
     vertical-align: baseline;
-    box-sizing: border-box;
   }
   /* HTML5 display-role reset for older browsers */
   article,
