c4becc2af008a74e5352e99c264fa82ac5205314
Cola <bcad1591@gmail.com>
2023-06-10 05:14:56 +0900

    feat: composeUrls 추가

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,3 +1,17 @@
 // eslint-disable-next-line
 type Noop = (...args: any[]) => any;
 export const noop: Noop = () => {};
+
+/**
+ * @example
+ *   composeUrls('http://example.com/', '/url')
+ *   composeUrls('http://example.com', 'url')
+ *   // http://example.com/url
+ */
+export const composeUrls = (...urls: string[]) => {
+  return urls
+    .map((url) => url.trim().replace(/(^\/+)|(\/+$)/g, ''))
+    .filter(Boolean)
+    .join('/')
+    .trim();
+};
