1920ce23247454c8a548724b267d9c175bc4d89b
Cola <bcad1591@gmail.com>
2023-06-10 05:41:49 +0900

    refactor: signIn api 업데이트

--- src/mocks/handlers/auth/index.ts
+++ src/mocks/handlers/auth/index.ts
@@ -1,24 +1,24 @@
-import type { ApiSuccessResponse } from '~/types';
+import type { SignInApiData } from '~/services/auth';
 
 import { rest } from 'msw';
 
 import { endpoints } from '~/react-query/common/queryKeys';
 import { API_URL, composeUrls } from '~/utils';
 
-interface SignInData {
-  accessToken: string;
-}
+const COOKIE_VALUE = '.';
 
-const signIn = rest.post<never, never, ApiSuccessResponse<SignInData>>(
+const signIn = rest.post<never, never, SignInApiData>(
   composeUrls(API_URL, endpoints.auth.signIn()),
   (req, res, ctx) => {
     return res(
+      ctx.cookie('accesstoken', COOKIE_VALUE, {}),
+      ctx.cookie('refreshtoken', COOKIE_VALUE, {
+        path: '/auth/reissue',
+      }),
       ctx.delay(500),
       ctx.json({
         statusCode: 200,
-        data: {
-          accessToken: 'accessToken',
-        },
+        data: {},
         message: '',
       })
     );
