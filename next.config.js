25da120026a46157b092fe07d5a562b54b956868
Cola <bcad1591@gmail.com>
2023-06-15 01:17:17 +0900

    chore: @svgr/webpack 세팅

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const svgrConfig = require('./config/svgr');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    svgrConfig(config);
+    return config;
+  },
   compiler: {
     emotion: {
       labelFormat: '[dirname]_[filename]_[local]',
