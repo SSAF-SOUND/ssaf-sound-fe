0944da56f0f68690b460853b72af815fc3d0e2b2
Cola <bcad1591@gmail.com>
2023-06-15 04:15:09 +0900

    feat: font 업데이트
    [skip ci]

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -4,13 +4,7 @@ import { Global, css } from '@emotion/react';
 import AppGlobalStyles from '../src/styles/GlobalStyles';
 
 const StorybookGlobalStyles = () => {
-  return (
-    <Global
-      styles={css`
-        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap');
-      `}
-    />
-  );
+  return <Global styles={css``} />;
 };
 
 const preview: Preview = {
---
+++ src/stories/Typography.stories.tsx
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { fontCss } from '~/styles/utils';
+
+interface TypographyProps {
+  language?: 'ko' | 'en';
+  color?: string;
+}
+
+const Typography = (props: TypographyProps) => {
+  const { language = 'ko', color = '#000000' } = props;
+  const text = language === 'ko' ? '쌉사운드' : 'SSAFSOUND';
+  return (
+    <div
+      css={
+        language === 'ko' ? fontCss.family.pretendard : fontCss.family.manrope
+      }
+    >
+      {Object.entries(fontCss.style).map(([styleName, css]) => {
+        return (
+          <p key={styleName} style={{ color }} css={css}>
+            {text}/{styleName}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
+
+const meta: Meta<typeof Typography> = {
+  title: 'System/Typography',
+  component: Typography,
+  argTypes: {
+    language: {
+      options: ['ko', 'en'],
+      control: {
+        type: 'radio',
+        labels: {
+          ko: '한국어',
+          en: '영어',
+        },
+      },
+    },
+  },
+};
+
+export default meta;
+
+type TypographyStory = StoryObj<typeof Typography>;
+
+export const Default: TypographyStory = {
+  args: {
+    language: 'ko',
+    color: '#000000',
+  },
+};
