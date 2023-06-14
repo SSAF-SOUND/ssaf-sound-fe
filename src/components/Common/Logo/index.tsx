5e0b05bb255799261bbd88269b3901addee409b9
Cola <bcad1591@gmail.com>
2023-06-15 01:17:54 +0900

    feat: Logo 추가
    [skip ci]

--- src/components/Common/Logo/index.tsx
+++ src/components/Common/Logo/index.tsx
@@ -1,9 +1,35 @@
-const Logo = () => {
+import { css } from '@emotion/react';
+import * as AccessibleIcon from '@radix-ui/react-accessible-icon';
+
+import LogoSVG from '~/assets/images/logo.svg';
+
+export type LogoSize = 'sm' | 'lg';
+
+export interface LogoProps {
+  size?: LogoSize;
+}
+
+const Logo = (props: LogoProps) => {
+  const { size = 'sm' } = props;
   return (
-    <div>
-      <h2>SSAFSOUND</h2>
-    </div>
+    <AccessibleIcon.Root label="로고">
+      <LogoSVG css={[baseCss, svgCss[size]]} />
+    </AccessibleIcon.Root>
   );
+};
+
+const baseCss = css({
+  width: 'auto',
+  pointerEvent: 'none',
+});
+
+const svgCss = {
+  sm: css({
+    height: 20,
+  }),
+  lg: css({
+    height: 32,
+  }),
 };
 
 export default Logo;
