380ce7d79e7b771d30870837e629c9481ae15602
Cola <bcad1591@gmail.com>
2023-06-16 05:02:40 +0900

    feat: Logo 업데이트
    - navigateToHome 추가

--- src/components/Common/Logo/index.tsx
+++ src/components/Common/Logo/index.tsx
@@ -1,26 +1,40 @@
+import type { MouseEventHandler } from 'react';
+
+import { useRouter } from 'next/router';
+
 import { css } from '@emotion/react';
 import * as AccessibleIcon from '@radix-ui/react-accessible-icon';
 
 import LogoSVG from '~/assets/images/logo.svg';
+import { inlineFlex } from '~/styles/utils';
 
 export type LogoSize = 'sm' | 'lg';
 
 export interface LogoProps {
   size?: LogoSize;
+  navigateToHome?: boolean;
 }
 
 const Logo = (props: LogoProps) => {
-  const { size = 'sm' } = props;
+  const { size = 'sm', navigateToHome = false } = props;
+  const router = useRouter();
+  const handleClick = () => {
+    if (navigateToHome) router.push('/');
+  };
+
   return (
-    <AccessibleIcon.Root label="로고">
-      <LogoSVG css={[baseCss, svgCss[size]]} />
-    </AccessibleIcon.Root>
+    <div css={[selfCss, navigateToHome && cursorCss]} onClick={handleClick}>
+      <AccessibleIcon.Root label="로고">
+        <LogoSVG css={[svgBaseCss, svgCss[size]]} />
+      </AccessibleIcon.Root>
+    </div>
   );
 };
 
-const baseCss = css({
+const selfCss = css(inlineFlex());
+
+const svgBaseCss = css({
   width: 'auto',
-  pointerEvent: 'none',
 });
 
 const svgCss = {
@@ -31,5 +45,9 @@ const svgCss = {
     height: 32,
   }),
 };
+
+const cursorCss = css({
+  cursor: 'pointer',
+});
 
 export default Logo;
