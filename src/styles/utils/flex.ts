1803413059981bfd5936a7718d436785024848a4
Cola <bcad1591@gmail.com>
2023-06-16 17:00:30 +0900

    feat: Flex 업데이트
    - gap, wrap 추가

--- src/styles/utils/flex.ts
+++ src/styles/utils/flex.ts
@@ -6,68 +6,85 @@ import { css } from '@emotion/react';
 type FlexDirection = CSSProperties['flexDirection'];
 type JustifyContent = CSSProperties['justifyContent'];
 type AlignItems = CSSProperties['alignItems'];
+type Gap = CSSProperties['gap'];
+type FlexWrap = CSSProperties['flexWrap'];
+
 interface FlexOptions {
   items?: AlignItems;
   justify?: JustifyContent;
   dir?: FlexDirection;
+  gap?: Gap;
+  wrap?: FlexWrap;
 }
 
 const flexCore = (
   alignItems: AlignItems | FlexOptions = '',
   justifyContent: JustifyContent = '',
-  flexDirection: FlexDirection = 'row'
+  flexDirection: FlexDirection = 'row',
+  gap: Gap = 0,
+  flexWrap: FlexWrap = 'nowrap'
 ) => {
-  const base = { display: 'flex' };
-
   if (typeof alignItems === 'string') {
-    return css(
-      {
-        alignItems,
-        justifyContent,
-        flexDirection,
-      },
-      base
-    );
+    return css({
+      alignItems,
+      justifyContent,
+      flexDirection,
+      gap,
+      flexWrap,
+    });
   }
 
   const flexOptions = alignItems;
 
-  return css(
-    {
-      alignItems: flexOptions?.items,
-      justifyContent: flexOptions?.justify,
-      flexDirection: flexOptions?.dir,
-    },
-    base
-  );
+  return css({
+    alignItems: flexOptions.items,
+    justifyContent: flexOptions.justify,
+    flexDirection: flexOptions.dir,
+    gap: flexOptions.gap,
+    flexWrap: flexOptions.wrap,
+  });
 };
 
 export function flex(
   alignItems?: AlignItems,
   justifyContent?: JustifyContent,
-  flexDirection?: FlexDirection
+  flexDirection?: FlexDirection,
+  gap?: Gap,
+  flexWrap?: FlexWrap
 ): SerializedStyles;
 export function flex(options?: FlexOptions): SerializedStyles;
 export function flex(
   alignItems: AlignItems | FlexOptions = '',
   justifyContent: JustifyContent = '',
-  flexDirection: FlexDirection = 'row'
+  flexDirection: FlexDirection = 'row',
+  gap: Gap = 0,
+  flexWrap: FlexWrap = 'nowrap'
 ) {
   const base = { display: 'flex' };
-  return css(flexCore(alignItems, justifyContent, flexDirection), base);
+  return css(
+    flexCore(alignItems, justifyContent, flexDirection, gap, flexWrap),
+    base
+  );
 }
 
 export function inlineFlex(
   alignItems?: AlignItems,
   justifyContent?: JustifyContent,
-  flexDirection?: FlexDirection
+  flexDirection?: FlexDirection,
+  gap?: Gap,
+  flexWrap?: FlexWrap
 ): SerializedStyles;
 export function inlineFlex(options?: FlexOptions): SerializedStyles;
 export function inlineFlex(
   alignItems: AlignItems | FlexOptions = '',
   justifyContent: JustifyContent = '',
-  flexDirection: FlexDirection = 'row'
+  flexDirection: FlexDirection = 'row',
+  gap: Gap = 0,
+  flexWrap: FlexWrap = 'nowrap'
 ) {
   const base = { display: 'inline-flex' };
-  return css(flexCore(alignItems, justifyContent, flexDirection), base);
+  return css(
+    flexCore(alignItems, justifyContent, flexDirection, gap, flexWrap),
+    base
+  );
 }
