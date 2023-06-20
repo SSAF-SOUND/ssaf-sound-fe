0d54dcdafb3bce415bf9eaa078ea2bf4988d3c39
otterp012 <otterp012@gmail.com>
2023-06-20 16:28:45 +0900

    feat: recruit msw handlers 작성

---
+++ src/mocks/handlers/recruit/index.tsx
@@ -0,0 +1,34 @@
+import type {
+  GetRecruitDetailApiData,
+  GetRecruitDetailData,
+} from '~/services/recruit';
+
+import { rest } from 'msw';
+
+import { mockSuccess } from '~/mocks/utils';
+import { endpoints } from '~/react-query/common';
+import { API_URL, composeUrls } from '~/utils';
+
+const getRecruitDetail = rest.get<never, never, GetRecruitDetailApiData>(
+  composeUrls(API_URL, endpoints.recruit.detail()),
+  (req, res, ctx) => {
+    console.log(req);
+    return res(
+      ctx.delay(500),
+      ...mockSuccess<GetRecruitDetailData>(ctx, {
+        category: 'study',
+        title: 'string',
+        recruitStart: new Date('2023-06-01'),
+        recruitEnd: new Date('2023-06-30'),
+        content: 'string',
+        created_at: new Date('2023-06-01'),
+        modified_at: new Date('2023-06-05'),
+        deleted_recruit: false,
+        finished_recruit: false,
+        view: 100,
+      })
+    );
+  }
+);
+
+export const recruitHandlers = [getRecruitDetail];
