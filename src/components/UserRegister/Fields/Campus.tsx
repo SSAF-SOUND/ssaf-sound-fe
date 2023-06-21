854557b87086ed5416aee2257bd45a3ceef1872d
Cola <bcad1591@gmail.com>
2023-06-21 18:47:00 +0900

    feat: UserRegister Campus 업데이트

--- src/components/UserRegister/Fields/Campus.tsx
+++ src/components/UserRegister/Fields/Campus.tsx
@@ -3,29 +3,31 @@ import { css } from '@emotion/react';
 import { SelectBox } from '~/components/Common';
 import { useSetPhaseContext } from '~/components/UserRegister/context';
 import Question from '~/components/UserRegister/Question';
+import { flex } from '~/styles/utils';
 
-const campuses = ['서울', '대전', '광주', '구미', '부산'];
+const campuses = ['서울', '대전', '광주', '구미', '부울경'];
 
 const Campus = () => {
   const setPhase = useSetPhaseContext();
   const handleValueChange = () => setPhase((p) => p + 1);
 
   return (
-    <div css={selfCss}>
+    <label css={selfCss}>
       <Question>
         <Question.Row>SSAFY</Question.Row>
         <Question.Row>캠퍼스를 선택해주세요</Question.Row>
       </Question>
 
-      <SelectBox items={campuses} onValueChange={handleValueChange} />
-    </div>
+      <SelectBox
+        items={campuses}
+        textAs={(value) => `${value} 캠퍼스`}
+        onValueChange={handleValueChange}
+        size="lg"
+      />
+    </label>
   );
 };
 
-const selfCss = css({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: 40,
-});
+const selfCss = css(flex('', '', 'column', 40));
 
 export default Campus;
