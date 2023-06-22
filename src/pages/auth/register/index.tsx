08cfef7de823be70e36247cfb176000e357b81f2
Cola <bcad1591@gmail.com>
2023-06-23 07:22:48 +0900

    feat: 등록이 필요없는 유저가 페이지 방문시 main으로 리다이렉션

--- src/pages/auth/register/index.tsx
+++ src/pages/auth/register/index.tsx
@@ -1,15 +1,26 @@
 import type { CustomNextPage } from 'next/types';
 
+import { useRouter } from 'next/router';
+
+import DefaultFullPageLoader from '~/components/Common/DefaultFullPageLoader';
 import UserRegister from '~/components/UserRegister';
+import { useMyAccountStatus } from '~/services/member';
 
 const RegisterPage: CustomNextPage = () => {
+  const router = useRouter();
+  const { isRegisterRequired } = useMyAccountStatus();
+
+  if (!isRegisterRequired) {
+    router.replace('/main');
+  }
+
   return <UserRegister />;
 };
 
 RegisterPage.auth = {
   role: 'user',
-  loading: <div>Loading</div>,
-  unauthorized: <div>Unauthorized</div>,
+  loading: <DefaultFullPageLoader text="유저 정보를 확인하는 중입니다." />,
+  unauthorized: '/main',
 };
 
 export default RegisterPage;
