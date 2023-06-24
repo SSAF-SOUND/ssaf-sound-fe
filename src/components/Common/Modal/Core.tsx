1a98155eb63818faafaa6cc85947a17f39d42a6c
Cola <bcad1591@gmail.com>
2023-06-25 02:09:56 +0900

    feat: Modal 업데이트 및 Controlled Modal 스토리 추가

--- src/components/Common/Modal/Core.tsx
+++ src/components/Common/Modal/Core.tsx
@@ -16,18 +16,38 @@ interface ModalCoreProps {
    */
   open?: boolean;
   /**
-   * - `uncontrolled`로 활용하는 경우에만 사용합니다.
-   * - 모달을 열기 위한 버튼 컴포넌트
+   * - `uncontrolled`로 활용하는 경우에는 이 버튼으로 `open`상태를 바꿀 수 있습니다.
    */
   trigger?: ReactElement;
   /**
    * - 기본값은 `true`이고, 콘텐츠의 부모 엘리먼트가 `body`가 됩니다.
    */
   portal?: boolean;
+  /**
+   * - 콘텐츠 바깥을 포인팅했을 때 수행할 콜백함수입니다.
+   * - `uncontrolled`에서는 `e.preventDefault()`를 사용하여 오버레이 클릭 시 모달 닫힘을 막을 수 있습니다.
+   */
+  onPointerDownOutside?: (
+    e: CustomEvent<{
+      originalEvent: PointerEvent;
+    }>
+  ) => void;
+  /**
+   * - `ESC`키 눌렀을 때 수행할 콜백함수입니다.
+   * - `uncontrolled`에서는 `e.preventDefault()`를 사용하여 `ESC`키를 누를시 모달 닫힘을 막을 수 있습니다.
+   */
+  onEscapeKeyDown?: (e: KeyboardEvent) => void;
 }
 
 const ModalCore = (props: ModalCoreProps) => {
-  const { content, open, trigger, portal = true } = props;
+  const {
+    content,
+    open,
+    trigger,
+    portal = true,
+    onPointerDownOutside,
+    onEscapeKeyDown,
+  } = props;
 
   return (
     <Dialog.Root open={open}>
@@ -35,12 +55,22 @@ const ModalCore = (props: ModalCoreProps) => {
       {portal ? (
         <Dialog.Portal>
           <Dialog.Overlay css={overlayCss.black} />
-          <Dialog.Content>{content}</Dialog.Content>
+          <Dialog.Content
+            onEscapeKeyDown={onEscapeKeyDown}
+            onPointerDownOutside={onPointerDownOutside}
+          >
+            {content}
+          </Dialog.Content>
         </Dialog.Portal>
       ) : (
         <>
           <Dialog.Overlay css={overlayCss.black} />
-          <Dialog.Content>{content}</Dialog.Content>
+          <Dialog.Content
+            onEscapeKeyDown={onEscapeKeyDown}
+            onPointerDownOutside={onPointerDownOutside}
+          >
+            {content}
+          </Dialog.Content>
         </>
       )}
     </Dialog.Root>
---
+++ src/components/Common/Modal/Modal.stories.tsx
@@ -0,0 +1,126 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { css } from '@emotion/react';
+import { useState } from 'react';
+
+import { Button } from '~/components/Common';
+import { flex, fontCss, palettes, position } from '~/styles/utils';
+
+import Modal from './index';
+
+const meta: Meta<typeof Modal> = {
+  title: 'Modal/Modal',
+  component: Modal,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type ModalStory = StoryObj<typeof Modal>;
+
+export const UncontrolledModal: ModalStory = {
+  render: () => {
+    const dataArr = ['A', 'B', 'C', 'D'];
+
+    return (
+      <div css={selfCss}>
+        {dataArr.map((data) => (
+          <Modal
+            key={data}
+            trigger={<Button>Remove {data}</Button>}
+            content={
+              <div css={contentCss}>
+                <Modal.Title>Title: {data}를 삭제합니다.</Modal.Title>
+                <Modal.Description>Description</Modal.Description>
+
+                <div>
+                  <Modal.Close>취소</Modal.Close>
+                  <Modal.Close onClick={() => console.log(`${data} 삭제`)}>
+                    삭제
+                  </Modal.Close>
+                </div>
+              </div>
+            }
+          />
+        ))}
+      </div>
+    );
+  },
+};
+
+export const ControlledModal: ModalStory = {
+  render: function Render() {
+    const [open, setOpen] = useState(false);
+    const dataArr = ['A', 'B', 'C', 'D'];
+
+    const [selectedData, setSelectedData] = useState('');
+
+    return (
+      <div css={selfCss}>
+        {dataArr.map((data) => (
+          <Button
+            key={data}
+            color="secondary"
+            onClick={() => {
+              setOpen(true);
+              setSelectedData(data);
+            }}
+          >
+            Remove {data}
+          </Button>
+        ))}
+
+        <Modal
+          open={open}
+          onEscapeKeyDown={() => setOpen(false)}
+          onPointerDownOutside={() => setOpen(false)}
+          content={
+            <div css={contentCss}>
+              <Modal.Title>Title: {selectedData}를 삭제합니다.</Modal.Title>
+              <Modal.Description>Description</Modal.Description>
+
+              <div>
+                <Modal.Close
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  취소
+                </Modal.Close>
+                <Modal.Close
+                  onClick={() => {
+                    setOpen(false);
+                    console.log(`${selectedData} 삭제`);
+                  }}
+                >
+                  삭제
+                </Modal.Close>
+              </div>
+            </div>
+          }
+        />
+      </div>
+    );
+  },
+};
+
+const selfCss = css(flex('flex-start', 'center', 'column', 10));
+
+const contentCss = css([
+  position.xy('center', 'center', 'fixed'),
+  flex('center', 'center', 'column', 10),
+  fontCss.family.auto,
+  {
+    background: 'white',
+    width: 200,
+    height: 200,
+    color: 'black',
+    '& button': {
+      width: 100,
+      color: palettes.primary.darken,
+    },
+    '& button:first-child': {
+      color: palettes.error.default,
+    },
+  },
+]);
