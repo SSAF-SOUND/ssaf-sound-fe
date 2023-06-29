6ed5d2523d29b3578059a0f74faada7b8c872a9f
otterp012 <otterp012@gmail.com>
2023-06-29 19:00:04 +0900

    fix: recruit API type에 skills, limits 추가

--- src/services/recruit/apis.ts
+++ src/services/recruit/apis.ts
@@ -5,17 +5,26 @@ import { privateAxios } from '~/utils';
 
 export interface GetRecruitDetailData {
   // /recruit
-  // 임시
+  // 나중에 공통되는 타입들을 정리하려고 합니다!
+  // 날짜 관련은 YY-MM-DD 스트링으로 주신다고 하셔서, string으로 갔습니다
   category: 'study' | 'project';
   title: string;
-  recruitStart: Date;
-  recruitEnd: Date;
+  recruitStart: string;
+  recruitEnd: string;
   content: string;
-  created_at: Date;
-  modified_at: Date;
+  created_at: string;
+  modified_at: string;
   deleted_recruit: boolean;
   finished_recruit: boolean;
   view: number;
+  skills: {
+    skillId: number;
+    name: string;
+  }[];
+  limits: {
+    recruitType: '기획/디자인' | '프론트엔드' | '백엔드';
+    limit: number;
+  }[];
 }
 
 export type GetRecruitDetailApiData = ApiSuccessResponse<GetRecruitDetailData>;
