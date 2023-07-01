73980812aab2706b57bd48cab73941ad9a0e02ad
Cola <bcad1591@gmail.com>
2023-07-02 06:45:55 +0900

    feat: toWebp 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,4 +4,6 @@ export * from './handleAxiosError';
 export * from './misc';
 export * from './number';
 export * from './webStorage';
+export * from './routes';
+export * from './toWebp';
 export { default as customToast } from './customToast';
