d993aa7929deaffee917177dcb231bc2c20640aa
otterp012 <otterp012@gmail.com>
2023-07-04 16:41:18 +0900

    feat: Recruit 관련 타입 정리

--- src/services/recruit/utils/types.ts
+++ src/services/recruit/utils/types.ts
@@ -1,1 +1,31 @@
 export type RecruitCategory = 'study' | 'project';
+
+export type SkillType =
+  | 'React'
+  | 'JavaScript'
+  | 'TypeScript'
+  | 'Vue'
+  | 'Svelte'
+  | 'NextJs'
+  | 'NodeJs'
+  | 'Java'
+  | 'Spring'
+  | 'Swift'
+  | 'Android'
+  | 'Figma'
+  | 'XD'
+  | 'Django'
+  | 'IOS'
+  | 'Flutter';
+
+export type RecruitType = '프론트엔드' | '백엔드' | '기획/디자인';
+
+export interface LimitType {
+  recruitType: RecruitType;
+  limit: number;
+}
+
+export interface SkillsType {
+  name: SkillType;
+  skillId: number;
+}
