bbff730d160cf2034619e8ce8643154752da38bd
Cola <bcad1591@gmail.com>
2023-07-06 06:29:30 +0900

    fix: stacks 오타

--- src/components/Common/SkillStack/SkillStack.stories.tsx
+++ src/components/Common/SkillStack/SkillStack.stories.tsx
@@ -11,7 +11,7 @@ const stacks = [
   'Svelte',
   'NextJs',
   'Java',
-  'Node',
+  'NodeJs',
   'Spring',
   'Django',
   'Android',
@@ -20,9 +20,10 @@ const stacks = [
   'Flutter',
   'XD',
   'Figma',
-];
+] as const;
+
 const meta: Meta<typeof SkillStack> = {
-  title: 'SkillStack',
+  title: 'Icon/SkillStack',
   component: SkillStack,
   tags: ['autodocs'],
   argTypes: {
@@ -36,15 +37,15 @@ const meta: Meta<typeof SkillStack> = {
 export default meta;
 
 export const Single = (props: SkillStackProps) => {
-  const { stack = 'React' } = props;
-  return <SkillStack stack={stack} />;
+  const { stack = 'React', size } = props;
+  return <SkillStack stack={stack} size={size} />;
 };
 
 export const All = () => {
   return (
     <div style={{ display: 'flex', width: '200px', flexWrap: 'wrap' }}>
       {stacks.map((s) => (
-        <SkillStack stack={s as SkillStackProps['stack']} key={s} />
+        <SkillStack stack={s} key={s} size={24} />
       ))}
     </div>
   );
