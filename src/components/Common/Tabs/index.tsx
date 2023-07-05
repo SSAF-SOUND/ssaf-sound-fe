b61d4cabc73a09f7b81bf7f761ecadb3a9345401
otterp012 <otterp012@gmail.com>
2023-07-05 16:41:40 +0900

    chore: Tabs export

---
+++ src/components/Common/Tabs/index.tsx
@@ -0,0 +1,16 @@
+import { Content, Root } from '@radix-ui/react-tabs';
+
+import List from './List';
+import Trigger from './Trigger';
+
+const Tabs = Object.assign(
+  {},
+  {
+    Root,
+    Content,
+    List,
+    Trigger,
+  }
+);
+
+export default Tabs;
