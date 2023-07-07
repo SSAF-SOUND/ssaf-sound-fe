deb266f9bda1a368afbfd19fd79287fc915e8437
Cola <bcad1591@gmail.com>
2023-07-08 05:27:18 +0900

    feat: Category 추가

--- src/components/RecruitForm/index.tsx
+++ src/components/RecruitForm/index.tsx
@@ -1,23 +1,41 @@
+import type { RecruitFormValues } from './type';
+
 import { FormProvider, useForm } from 'react-hook-form';
 
 import SubmitBar from '~/components/RecruitForm/SubmitBar';
 
+import { Category } from './Fields';
+
 interface RecruitFormOptions {
+  // SubmitBar
   barTitle: string;
   submitButtonText: string;
+
+  // Category
+  isProjectDisabled: boolean;
+  isStudyDisabled: boolean;
 }
 
 interface RecruitFormProps {
   options?: Partial<RecruitFormOptions>;
 }
 
+const defaultValues = {
+  category: '프로젝트',
+};
+
 const RecruitForm = (props: RecruitFormProps) => {
   const { options = {} } = props;
 
-  const { barTitle = '', submitButtonText = '' } = options;
+  const {
+    barTitle = '',
+    submitButtonText = '',
+    isProjectDisabled,
+    isStudyDisabled,
+  } = options;
 
-  const methods = useForm({
-    defaultValues: {},
+  const methods = useForm<RecruitFormValues>({
+    defaultValues,
   });
   const { handleSubmit } = methods;
   const onValid = (value: unknown) => console.log(value);
@@ -26,6 +44,10 @@ const RecruitForm = (props: RecruitFormProps) => {
     <FormProvider {...methods}>
       <form onSubmit={handleSubmit(onValid)}>
         <SubmitBar title={barTitle} submitButtonText={submitButtonText} />
+        <Category
+          isProjectDisabled={isProjectDisabled}
+          isStudyDisabled={isStudyDisabled}
+        />
       </form>
     </FormProvider>
   );
---
+++ src/components/RecruitForm/type.ts
@@ -0,0 +1,3 @@
+export interface RecruitFormValues {
+  category: string;
+}
