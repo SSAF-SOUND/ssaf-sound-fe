2135e247aee9eee3e1e8937ce4f2c7bd36e7981a
Cola <bcad1591@gmail.com>
2023-07-10 23:14:24 +0900

    refactor: 불필요한 import 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,13 @@
-import type { GetServerSideProps } from 'next/types';
-
-import Head from 'next/head';
 import { useRouter } from 'next/router';
 
-import { BounceLoader } from 'react-spinners';
-
-import TopBar from 'src/components/TopBar';
 import { DefaultFullPageLoader } from '~/components/Common';
-import Counter from '~/components/Counter';
-import TitleBar from '~/components/TitleBar';
-import { prefetch } from '~/react-query/server/prefetch';
 import { useMyAccountStatus, useMyInfo } from '~/services/member';
-import { flex, palettes } from '~/styles/utils';
 import { webStorage } from '~/utils/webStorage';
 
 const HomePage = () => {
   const router = useRouter();
-  const { isFetching, data } = useMyInfo({ retry: 1 });
-  const { isRegisterRequired, isChecking } = useMyAccountStatus();
+  const { isFetching } = useMyInfo({ retry: 1 });
+  const { isRegisterRequired } = useMyAccountStatus();
   const returnPage = webStorage.getReturnPage();
 
   if (isFetching) {
