50311925b8415bbd80b4645ad6445f610973c311
Cola <bcad1591@gmail.com>
2023-07-11 20:49:45 +0900

    feat: Form 필드 구성

--- src/components/StudentCertificationForm/index.tsx
+++ src/components/StudentCertificationForm/index.tsx
@@ -1,9 +1,14 @@
 import type { StudentCertificationFormValues } from './utils';
 import type { SubmitHandler } from 'react-hook-form';
 
+import { useMemo } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 
-import { Quiz } from './Fields';
+import TitleBar from '~/components/TitleBar';
+import { useStack } from '~/hooks';
+import { noop } from '~/utils';
+
+import { Quiz, Track } from './Fields';
 
 interface StudentCertificationFormProps {
   onSubmit?: SubmitHandler<StudentCertificationFormValues>;
@@ -11,15 +16,32 @@ interface StudentCertificationFormProps {
 }
 
 const StudentCertificationForm = (props: StudentCertificationFormProps) => {
-  const { defaultValues = defaultStudentCertificationFormValues } = props;
+  const {
+    onSubmit = noop,
+    defaultValues = defaultStudentCertificationFormValues,
+  } = props;
   const methods = useForm<StudentCertificationFormValues>({
     defaultValues,
   });
 
+  const { push, pop, top } = useStack([0], { defaultTop: 0 });
+
+  const FieldComponents = useMemo(() => {
+    return [() => <Track onSelect={() => push(top + 1)} />, () => <Quiz />];
+  }, [push, top]);
+
+  const FieldComponent = FieldComponents[top];
+
   return (
     <FormProvider {...methods}>
-      <form>
-        <Quiz />
+      <TitleBar.Default
+        title="SSAFY 재학생 인증"
+        withoutBackward={top === 0}
+        withoutClose
+        onClickBackward={pop}
+      />
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+        <FieldComponent />
       </form>
     </FormProvider>
   );
