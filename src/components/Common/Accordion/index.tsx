a0fce21690557d6d5f9f4e99d6a62c1b8611513c
otterp012 <otterp012@gmail.com>
2023-07-11 18:36:09 +0900

    feat: Accrodion 추가

---
+++ src/components/Common/Accordion/index.tsx
@@ -0,0 +1,13 @@
+import * as RadixAccordion from '@radix-ui/react-accordion';
+
+import AccordionContent from './Content';
+import AccordionTrigger from './Trigger';
+
+const Accordion = {
+  Root: RadixAccordion.Root,
+  Item: RadixAccordion.Item,
+  Trigger: AccordionTrigger,
+  Content: AccordionContent,
+};
+
+export default Accordion;
