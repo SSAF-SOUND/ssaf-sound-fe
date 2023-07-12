c4edaa85e9a837f99b5a937d4b0df2f6287c82a5
otterp012 <otterp012@gmail.com>
2023-07-13 01:35:51 +0900

    feat: Recruits 타입, 엔드포인트 추가

--- src/services/recruit/apis.ts
+++ src/services/recruit/apis.ts
@@ -30,3 +30,34 @@ export const getRecruitDetail = () => {
   const endpoint = endpoints.recruit.detail();
   return privateAxios.get<GetRecruitDetailApiData>(endpoint).then((res) => res);
 };
+
+// 추후 보완할 예정입니다!
+// Recruits 전체를 불러오는 부분입니다
+
+export type GetRecruitsApiData = ApiSuccessResponse<RecruitsParams>;
+
+export interface RecruitsParams {
+  recruitId: number;
+  title: string;
+  finishedRecruit: boolean;
+  recruitEnd: string;
+  skills: SkillsType[];
+  participants: RecruitParticipantParams[];
+  currentPage: number;
+  totalPages: number;
+  lastPage: boolean;
+}
+
+export interface RecruitParticipantParams {
+  recruitType: RecruitType;
+  limit: number;
+  members: {
+    nickName: string;
+    major: boolean;
+  }[];
+}
+
+export const getRecruits = () => {
+  const endpoint = endpoints.recruit.data();
+  return privateAxios.get<GetRecruitsApiData>(endpoint).then((res) => res);
+};
