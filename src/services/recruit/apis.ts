ea4209c2b642e6a29cc28892c1b33dc02cca73b7
otterp012 <otterp012@gmail.com>
2023-07-13 01:35:06 +0900

    fix: RecruitDetail 타입 수정

--- src/services/recruit/apis.ts
+++ src/services/recruit/apis.ts
@@ -1,33 +1,30 @@
+import type {
+  LimitType,
+  RecruitCategoryType,
+  RecruitType,
+  SkillsType,
+} from './utils';
 import type { ApiSuccessResponse } from '~/types';
 
 import { endpoints } from '~/react-query/common';
 import { privateAxios } from '~/utils';
 
-export interface GetRecruitDetailData {
-  // /recruit
-  // 나중에 공통되는 타입들을 정리하려고 합니다!
-  // 날짜 관련은 YY-MM-DD 스트링으로 주신다고 하셔서, string으로 갔습니다
-  category: 'study' | 'project';
+export interface RecruitDetailParams {
+  category: RecruitCategoryType;
   title: string;
   recruitStart: string;
   recruitEnd: string;
   content: string;
-  created_at: string;
-  modified_at: string;
-  deleted_recruit: boolean;
-  finished_recruit: boolean;
+  createdAt: string;
+  modifiedAt: string;
+  deletedRecruit: boolean;
+  finishedRecruit: boolean;
   view: number;
-  skills: {
-    skillId: number;
-    name: string;
-  }[];
-  limits: {
-    recruitType: '기획/디자인' | '프론트엔드' | '백엔드';
-    limit: number;
-  }[];
+  skills: SkillsType[];
+  limits: LimitType[];
 }
 
-export type GetRecruitDetailApiData = ApiSuccessResponse<GetRecruitDetailData>;
+export type GetRecruitDetailApiData = ApiSuccessResponse<RecruitDetailParams>;
 
 export const getRecruitDetail = () => {
   const endpoint = endpoints.recruit.detail();
