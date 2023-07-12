95a15290769f90a98e09bd8f38d16ee20defdfdc
otterp012 <otterp012@gmail.com>
2023-07-13 01:48:49 +0900

    feat: Recruits Mocking 함수 추가

--- src/mocks/handlers/recruit/index.tsx
+++ src/mocks/handlers/recruit/index.tsx
@@ -1,6 +1,10 @@
 import type {
   GetRecruitDetailApiData,
+  GetRecruitsApiData,
   RecruitDetailParams,
+  RecruitParticipantParams,
+  RecruitsParams,
+  SkillsType,
 } from '~/services/recruit';
 
 import { rest } from 'msw';
@@ -50,4 +54,52 @@ const getRecruitDetail = rest.get<never, never, GetRecruitDetailApiData>(
   }
 );
 
-export const recruitHandlers = [getRecruitDetail];
+const data = {
+  recruits: [
+    {
+      recruitId: 1,
+      title: '제목1',
+      finishedRecruit: true,
+      recruitEnd: '2023-07-02',
+      content: '나른이른린릔르니ㅏ른라ㅣㄴㄴ',
+      skills: [
+        {
+          skillId: 1,
+          name: 'Spring',
+        },
+        {
+          skillId: 2,
+          name: 'React',
+        },
+      ] as unknown as SkillsType[],
+      participants: [
+        {
+          recruitType: '기획/디자인',
+          limit: 3,
+          members: [
+            {
+              nickName: 'khs',
+              major: true,
+            },
+            {
+              nickName: 'kds',
+              major: true,
+            },
+          ],
+        },
+      ] as unknown as RecruitParticipantParams[],
+    },
+  ],
+  currentPage: 0,
+  totalPages: 1,
+  lastPage: true,
+};
+
+const getRecruits = rest.get<never, never, GetRecruitsApiData>(
+  composeUrls(API_URL, endpoints.recruit.data()),
+  (req, res, ctx) => {
+    return res(ctx.delay(500), ...mockSuccess<RecruitsParams>(ctx, data));
+  }
+);
+
+export const recruitHandlers = [getRecruitDetail, getRecruits];
