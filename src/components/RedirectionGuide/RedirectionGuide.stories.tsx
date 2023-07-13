303649cadb6378e268f5c0e458f247bf3736d6a2
Cola <bcad1591@gmail.com>
2023-07-14 05:30:41 +0900

    feat: RedirectionGuide 스토리 추가

---
+++ src/components/RedirectionGuide/RedirectionGuide.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { PageLayout } from '~/stories/Layout';
+import { disableArgs } from '~/stories/utils';
+
+import RedirectionGuide from './index';
+
+const meta: Meta<typeof RedirectionGuide> = {
+  title: 'RedirectionGuide',
+  component: RedirectionGuide,
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <PageLayout>
+        <Story />
+      </PageLayout>
+    ),
+  ],
+  argTypes: {
+    title: {
+      type: 'string',
+    },
+    description: {
+      type: 'string',
+    },
+    redirectionText: {
+      type: 'string',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof RedirectionGuide>;
+
+export const Default: Story = {
+  args: {
+    theme: 'primary',
+    title: 'Title',
+    description: 'Description',
+    redirectionText: 'Redirection Text',
+    redirectionTo: '/',
+  },
+  render: (args) => {
+    return <RedirectionGuide {...args} indicator={undefined} />;
+  },
+};
