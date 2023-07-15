e1236ea6417556ff7e1fe3cb4f724b55471b98f8
Cola <bcad1591@gmail.com>
2023-07-16 01:09:54 +0900

    feat: setting, document 아이콘 추가

--- src/components/Common/Icon/index.tsx
+++ src/components/Common/Icon/index.tsx
@@ -24,6 +24,7 @@ import {
   MdClose,
   MdControlPoint,
   MdCreate,
+  MdDescription,
   MdGroup,
   MdGroupAdd,
   MdHome,
@@ -35,6 +36,7 @@ import {
   MdOutlineThumbUp,
   MdRefresh,
   MdRemoveCircleOutline,
+  MdSettings,
   MdThumbUp,
 } from 'react-icons/md';
 import { RiKakaoTalkFill } from 'react-icons/ri';
@@ -86,6 +88,8 @@ export const icons = {
 
   image: <BiImageAdd />,
   link: <MdLink />,
+  setting: <MdSettings />,
+  document: <MdDescription />,
 
   google: <AiOutlineGoogle />,
   github: <AiFillGithub />,
