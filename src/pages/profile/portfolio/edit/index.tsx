c2de66aeb5e6b88707fa074b5d0fa220409bbf9c
Cola <bcad1591@gmail.com>
2023-07-17 00:33:01 +0900

    feat: 포트폴리오 수정 페이지 업데이트

--- src/pages/profile/portfolio/edit/index.tsx
+++ src/pages/profile/portfolio/edit/index.tsx
@@ -1,15 +1,36 @@
 import type { CustomNextPage } from 'next/types';
+import type { PortfolioFormProps } from '~/components/PortfolioForm';
+
+import { css } from '@emotion/react';
 
 import { DefaultFullPageLoader } from '~/components/Common';
 import PortfolioForm from '~/components/PortfolioForm';
-import { routes } from '~/utils';
+import { customToast, routes } from '~/utils';
 
-interface PortfolioEditPageProps {}
+// TODO
+//   ValidSubmitHandler
+//   defaultValues 반영 (값을 수정할 때)
 
-const PortfolioEditPage: CustomNextPage = (props: PortfolioEditPageProps) => {
+const PortfolioEditPage: CustomNextPage = () => {
+  const onInvalidSubmit: PortfolioFormProps['onInvalidSubmit'] = (errors) => {
+    const errorMessage =
+      errors?.links?.[0]?.link?.message ||
+      errors?.links?.[0]?.linkText?.message ||
+      errors?.selfIntroduction?.message;
+
+    if (errorMessage) customToast.clientError(errorMessage);
+  };
+
+  const onValidSubmit: PortfolioFormProps['onValidSubmit'] = (value) => {
+    console.log(value);
+  };
+
   return (
-    <div>
-      <PortfolioForm />
+    <div css={selfCss}>
+      <PortfolioForm
+        onInvalidSubmit={onInvalidSubmit}
+        onValidSubmit={onValidSubmit}
+      />
     </div>
   );
 };
@@ -20,3 +41,7 @@ PortfolioEditPage.auth = {
   loading: <DefaultFullPageLoader />,
   unauthorized: routes.unauthorized(),
 };
+
+const selfCss = css({
+  padding: '0 15px',
+});
