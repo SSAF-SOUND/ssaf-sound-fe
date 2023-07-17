afcd5463a001b4a84d819718e55bb95d3949f1db
otterp012 <otterp012@gmail.com>
2023-07-17 20:22:38 +0900

    fix: Dday Size 추가

--- src/components/Dday/index.tsx
+++ src/components/Dday/index.tsx
@@ -1,3 +1,5 @@
+import type { SerializedStyles } from '@emotion/react';
+import type { ComponentPropsWithoutRef } from 'react';
 import type { RecruitCategory } from '~/services/recruit';
 
 import { css } from '@emotion/react';
@@ -6,27 +8,44 @@ import { getRecruitThemeByCategory } from '~/services/recruit';
 import { fontCss, themeColorVars } from '~/styles/utils';
 import { getDateDiff } from '~/utils';
 
-export interface DdayProps {
+export interface DdayProps extends ComponentPropsWithoutRef<'span'> {
   recruitEnd: string;
   category: RecruitCategory;
+  size?: 'sm' | 'md';
 }
 
+type DdaySizes = 'sm' | 'md';
+
 const Dday = (props: DdayProps) => {
-  const { recruitEnd = '2023-06-30', category = 'study' } = props;
+  const {
+    recruitEnd = '2023-06-30',
+    category = 'project',
+    size = 'md',
+    ...restProps
+  } = props;
 
   const diff = getDateDiff(new Date(recruitEnd));
   const isAbleDate = diff >= 0;
   const dDay = isAbleDate ? `D - ${diff}` : '모집 완료';
 
   return (
-    <span css={[textCss]} data-theme={getRecruitThemeByCategory(category)}>
+    <span
+      css={[textCss, sizeCss[size]]}
+      data-theme={getRecruitThemeByCategory(category)}
+      {...restProps}
+    >
       {dDay}
     </span>
   );
 };
 
-const textCss = css(fontCss.style.B20, {
+const textCss = css(fontCss.family.auto, {
   color: themeColorVars.mainColor.var,
 });
+
+const sizeCss: Record<DdaySizes, SerializedStyles> = {
+  sm: css(fontCss.style.B16),
+  md: css(fontCss.style.B20),
+};
 
 export default Dday;
