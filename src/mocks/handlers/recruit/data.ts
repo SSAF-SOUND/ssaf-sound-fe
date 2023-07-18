7078f2e0ba08126e74fe1796870264df100ce798
otterp012 <otterp012@gmail.com>
2023-07-19 00:41:43 +0900

    fix: Recruit mock 데이터 추가

--- src/mocks/handlers/recruit/data.ts
+++ src/mocks/handlers/recruit/data.ts
@@ -1,26 +1,123 @@
-export const data = {
-  '1': {
+import type {
+  RecruitCategory,
+  Recruits,
+  RecruitDetail,
+  SkillsType,
+  RecruitParticipant,
+  RecruitSummary,
+  RecruitMembers,
+} from '~/services/recruit';
+
+import { userInfo } from '../member/data';
+
+const recruitDetail: Record<RecruitCategory, RecruitDetail> = {
+  study: {
     category: 'study',
     title: 'string',
-    recruitStart: new Date('2023-06-01'),
-    recruitEnd: new Date('2023-06-30'),
+    recruitStart: '2023-06-01',
+    recruitEnd: '2023-06-30',
     content: 'string',
-    created_at: new Date('2023-06-01'),
-    modified_at: new Date('2023-06-05'),
-    deleted_recruit: false,
-    finished_recruit: false,
+    createdAt: '2023-06-01',
+    modifiedAt: '2023-06-05',
+    deletedRecruit: false,
+    finishedRecruit: false,
     view: 100,
+    skills: [],
+    limits: [],
   },
-  '2': {
+  project: {
     category: 'project',
     title: 'prject test',
-    recruitStart: new Date('2023-06-12'),
-    recruitEnd: new Date('2023-06-15'),
+    recruitStart: '2023-06-12',
+    recruitEnd: '2023-06-15',
     content: 'tttttt',
-    created_at: new Date('2023-06-07'),
-    modified_at: new Date('2023-06-05'),
-    deleted_recruit: false,
-    finished_recruit: true,
+    createdAt: '2023-06-07',
+    modifiedAt: '2023-06-05',
+    deletedRecruit: false,
+    finishedRecruit: true,
     view: 100,
+    skills: [
+      {
+        skillId: 1,
+        name: 'Spring',
+      },
+      {
+        skillId: 2,
+        name: 'React',
+      },
+    ],
+    limits: [
+      {
+        recruitType: '기획/디자인',
+        limit: 2,
+      },
+      {
+        recruitType: '프론트엔드',
+        limit: 3,
+      },
+    ],
   },
+};
+
+const recruitSummary: RecruitSummary = {
+  recruitId: 1,
+  title: '제목1',
+  finishedRecruit: true,
+  recruitEnd: '2023-07-02',
+  skills: [
+    {
+      skillId: 1,
+      name: 'Spring',
+    },
+    {
+      skillId: 2,
+      name: 'React',
+    },
+  ] as unknown as SkillsType[],
+  participants: [
+    {
+      recruitType: '기획/디자인',
+      limit: 3,
+      members: [
+        {
+          nickName: 'khs',
+          major: true,
+        },
+        {
+          nickName: 'kds',
+          major: true,
+        },
+      ],
+    },
+  ] as unknown as RecruitParticipant[],
+};
+
+const recruits: Recruits = {
+  recruits: [recruitSummary],
+  currentPage: 0,
+  totalPages: 1,
+  lastPage: true,
+};
+
+const recruitMembers: RecruitMembers = {
+  members: [
+    {
+      recruitType: '기획/디자인',
+      ...userInfo.certifiedSsafyUserInfo,
+    },
+    {
+      recruitType: '프론트엔드',
+      ...userInfo.uncertifiedSsafyUserInfo,
+    },
+    {
+      recruitType: '백엔드',
+      ...userInfo.nonSsafyUserInfo,
+    },
+  ],
+};
+export const RecruitData = {
+  recruitDetail,
+  recruits,
+  recruitSummary,
+  recruitMembers,
 };
