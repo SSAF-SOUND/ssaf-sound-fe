1d89d22279bf06a1e188c16c9f23a93c5eb255d9
Cola <bcad1591@gmail.com>
2023-07-22 21:08:43 +0900

    feat: TrackEditPage Story 추가

---
+++ src/stories/TrackEditPage.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { useEffect } from 'react';
+
+import { updateTrack } from '~/mocks/handlers/member';
+import { userInfo } from '~/mocks/handlers/member/data';
+import MyInfoSettingsTrackEditPage from '~/pages/profile/myinfo-settings/track/edit';
+import { useSetMyInfo } from '~/services/member';
+import { PageLayout } from '~/stories/Layout';
+
+const meta: Meta<typeof MyInfoSettingsTrackEditPage> = {
+  title: 'Page/MyInfoSettings/TrackEdit',
+  component: MyInfoSettingsTrackEditPage,
+  decorators: [
+    (Story) => (
+      <PageLayout>
+        <Story />
+      </PageLayout>
+    ),
+  ],
+  parameters: {
+    layout: 'fullscreen',
+
+    msw: {
+      handlers: {
+        member: [updateTrack],
+      },
+    },
+  },
+};
+
+export default meta;
+
+type TrackEditPageStory = StoryObj<typeof MyInfoSettingsTrackEditPage>;
+
+export const Default: TrackEditPageStory = {
+  decorators: [
+    (Story) => {
+      const setMyInfo = useSetMyInfo();
+      useEffect(() => {
+        setMyInfo(userInfo.certifiedSsafyUserInfo);
+      }, [setMyInfo]);
+
+      return <Story />;
+    },
+  ],
+};
