99e04a92e4ca1588136244ba1e61e446c877b315
Cola <bcad1591@gmail.com>
2023-07-24 06:54:09 +0900

    feat: ArticleCategoryCard 추가

---
+++ src/components/ArticleCategoryCard/ArticleCategoryCardSkeleton.tsx
@@ -0,0 +1,16 @@
+import Skeleton from 'react-loading-skeleton';
+
+import { palettes } from '~/styles/utils';
+
+const ArticleCategoryCardSkeleton = () => {
+  return (
+    <Skeleton
+      borderRadius={20}
+      height={116}
+      baseColor={palettes.grey3}
+      highlightColor={palettes.grey5}
+    />
+  );
+};
+
+export default ArticleCategoryCardSkeleton;
