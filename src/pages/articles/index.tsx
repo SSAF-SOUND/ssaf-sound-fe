807f09b8620f025a8ff905481850dca0c58dbebe
Cola <bcad1591@gmail.com>
2023-07-24 16:33:54 +0900

    feat: Articles 페이지

---
+++ src/pages/articles/index.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link';
+
+import { css } from '@emotion/react';
+
+import { ArticleCategoryCard } from '~/components/ArticleCategoryCard';
+import NavigationGroup from '~/components/NavigationGroup';
+import { useArticleCategories } from '~/services/articles';
+import { flex, fontCss, palettes, topBarHeight } from '~/styles/utils';
+
+const ArticlesPage = () => {
+  const { data: articleCategories } = useArticleCategories();
+
+  return (
+    <div css={selfCss}>
+      <div css={[descriptionCss, { marginBottom: 24 }]}>
+        <span>모아보기</span>
+        <Link href="/" css={hotLinkCss}>
+          Hot
+        </Link>
+      </div>
+      <div css={categoriesCss}>
+        {!articleCategories ? (
+          <>
+            {Array(6)
+              .fill(undefined)
+              .map((_, i) => (
+                <ArticleCategoryCard.Skeleton key={i} />
+              ))}
+          </>
+        ) : (
+          articleCategories.map((articleCategory) => (
+            <ArticleCategoryCard
+              key={articleCategory.boardId}
+              articleCategory={articleCategory}
+            />
+          ))
+        )}
+      </div>
+      <NavigationGroup />
+    </div>
+  );
+};
+
+export default ArticlesPage;
+
+const selfCss = css({ padding: `${topBarHeight + 40}px 15px` });
+
+const descriptionCss = css(
+  flex('center', 'space-between', 'row'),
+  fontCss.style.B16
+);
+
+const hotLinkCss = css({
+  color: palettes.secondary.dark,
+});
+
+const categoriesCss = css(flex('', '', 'column', 12));
