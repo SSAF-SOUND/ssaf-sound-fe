868f03aabd49583f3c49c01085b2889ef83d2a87
Cola <bcad1591@gmail.com>
2023-07-24 23:32:59 +0900

    chore: setup sentry

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { withSentryConfig } = require('@sentry/nextjs');
+
 const svgrConfig = require('./config/svgr');
 
 /** @type {import('next').NextConfig} */
@@ -10,9 +12,44 @@ const nextConfig = {
   },
   compiler: {
     emotion: {
-      labelFormat: '[dirname]_[filename]_[local]',
+      labelFormat: '[filename]_[local]',
     },
   },
 };
 
 module.exports = nextConfig;
+
+// Injected content via Sentry wizard below
+
+module.exports = withSentryConfig(
+  module.exports,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+
+    org: 'mjsdo',
+    project: 'ssaf-sound-fe',
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: false,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: '/monitoring',
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
+);
