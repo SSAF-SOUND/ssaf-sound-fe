d473ecc81c2027bedde62f7999ecfbafbd1b4e01
Cola <bcad1591@gmail.com>
2023-07-26 04:49:25 +0900

    fix: Blob 대신 File 반환, src는 변환된 이미지의 url을 반환

--- src/utils/toWebp.ts
+++ src/utils/toWebp.ts
@@ -19,49 +19,54 @@ export interface ToWebpOptions {
 type ToWebp = (
   file: File,
   options?: Partial<ToWebpOptions>
-) => Promise<{ blob: Blob; src: string }>;
+) => Promise<{ file: File; src: string }>;
 
 /* https://github.com/juunini/webp-converter-browser/blob/main/src/index.ts */
 export const toWebp: ToWebp = (file, options = {}) =>
   new Promise((resolve, reject) => {
     const { maxWebpSize = MB, quality = 0.8 } = options;
 
-    if (
-      ![
-        'image/webp',
-        'image/png',
-        'image/jpg',
-        'image/jpeg',
-        'image/gif',
-      ].includes(file.type)
-    ) {
+    if (!ALLOWED_IMAGE_TYPES.includes(file.type)) {
       reject('파일 타입이 유효하지 않습니다.');
       return;
     }
 
     const URL = window.URL;
-    const objectUrl = URL.createObjectURL(file);
+    const originalObjectUrl = URL.createObjectURL(file);
+    const originalFileName = file.name;
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d') as CanvasRenderingContext2D;
     const image = new Image();
 
-    image.src = objectUrl;
+    image.src = originalObjectUrl;
     image.crossOrigin = 'anonymous';
     image.onerror = () => reject('이미지 로드에 실패했습니다.');
     image.onload = () => {
       canvas.width = image.width;
       canvas.height = image.height;
       context.drawImage(image, 0, 0, canvas.width, canvas.height);
+      URL.revokeObjectURL(originalObjectUrl);
+
       canvas.toBlob(
         (data) => {
           const blob = data as Blob;
+
           if (blob.size > maxWebpSize) {
             reject(
               `파일 크기는 ${byteToMB(maxWebpSize)}MB 보다 작아야 합니다.`
             );
           }
 
-          resolve({ blob, src: objectUrl });
+          const fileName = originalFileName.replace(
+            /(png|jpe?g|gif|webp)$/i,
+            'webp'
+          );
+
+          const file = new File([blob], fileName, {
+            type: 'image/webp',
+          });
+
+          resolve({ file, src: URL.createObjectURL(file) });
         },
         'image/webp',
         quality
