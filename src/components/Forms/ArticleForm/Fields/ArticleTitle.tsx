a9edec8a45ad97bb40b5a7b5ff50dd1c67171fb5
Cola <bcad1591@gmail.com>
2023-07-28 02:28:50 +0900

    feat: 매직넘버 제거, placeholder, 에러메세지 추가

--- src/components/Forms/ArticleForm/Fields/ArticleTitle.tsx
+++ src/components/Forms/ArticleForm/Fields/ArticleTitle.tsx
@@ -1,10 +1,17 @@
+import { css } from '@emotion/react';
+import { ErrorMessage } from '@hookform/error-message';
+
+import { AlertText } from '~/components/Common';
 import Editor from '~/components/Editor';
 import { useArticleFormContext } from '~/components/Forms/ArticleForm/utils';
+import { palettes } from '~/styles/utils';
 
 const fieldName = 'title';
+const minTitleLength = 2;
+const maxTitleLength = 100;
 const validateTitle = (value: string) => {
-  if (value.length < 2 || value.length > 100)
-    return '제목은 2~100자 사이여야 합니다.';
+  if (value.length < minTitleLength || value.length > maxTitleLength)
+    return `제목은 ${minTitleLength}~${maxTitleLength}자 사이여야 합니다.`;
 
   return true;
 };
@@ -16,11 +23,31 @@ export const ArticleTitle = () => {
   } = useArticleFormContext();
 
   return (
-    <Editor.TitleInput
-      defaultValue={defaultTitle}
-      {...register(fieldName, {
-        validate: validateTitle,
-      })}
-    />
+    <div>
+      <Editor.TitleInput
+        placeholder="제목"
+        defaultValue={defaultTitle}
+        {...register(fieldName, {
+          validate: validateTitle,
+        })}
+      />
+      <ErrorMessage
+        name={fieldName}
+        render={({ message }) => {
+          return (
+            <Editor.MessageBox css={errorMessageContainerCss}>
+              <AlertText>{message}</AlertText>
+            </Editor.MessageBox>
+          );
+        }}
+      />
+    </div>
   );
 };
+
+const errorMessageContainerCss = css({
+  borderTop: 0,
+  borderBottom: 0,
+  padding: '4px 16px',
+  backgroundColor: palettes.white,
+});
