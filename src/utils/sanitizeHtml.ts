186b5d7575a379d410ad7a59a5a9216428fc3f9f
Cola <bcad1591@gmail.com>
2023-07-31 08:36:46 +0900

    feat: sanitize 이후 target 어트리뷰트를 _blank로 업데이트

--- src/utils/sanitizeHtml.ts
+++ src/utils/sanitizeHtml.ts
@@ -1,5 +1,19 @@
 import DOMPurify from 'dompurify';
 
+import { isClient } from '~/utils/misc';
+
+const addHook = () => {
+  if (isClient) {
+    DOMPurify.addHook('afterSanitizeAttributes', function (node) {
+      if ('target' in node) {
+        node.setAttribute('target', '_blank');
+      }
+    });
+  }
+};
+
+addHook();
+
 export const sanitizeHtml = (dirty: string) => {
   return DOMPurify.sanitize(dirty, { USE_PROFILES: { html: true } });
 };
