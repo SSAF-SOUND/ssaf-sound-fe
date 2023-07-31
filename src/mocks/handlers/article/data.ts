08b7615c1c63bd90d0cdca4a4e28ebfe14520355
Cola <bcad1591@gmail.com>
2023-08-01 06:58:02 +0900

    chore: mock html data에 white space 추가

--- src/mocks/handlers/article/data.ts
+++ src/mocks/handlers/article/data.ts
@@ -35,7 +35,33 @@ export const createMockArticle = (id: number): ArticleDetail => {
   const numberRange = { min: 1000, max: 100000 };
   return {
     title: `${id}번째 게시글`,
-    content: `<p><strong>BOLD</strong></p><p><u>UNDERLINE</u></p><p><code>inline-code</code></p><pre class="ql-syntax" spellcheck="false">code-block</pre><p><br></p><ol><li>ordered</li><li>ordered</li><li>ordered</li></ol><p><br></p><ul><li>unordered</li><li>unordered</li><li>unordered</li></ul><p><br></p><p><a href="https://www.naver.com" rel="noopener noreferrer" target="_blank">Link</a></p><p><br></p><h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3><p>Normal</p>`,
+    content: `
+        <p>
+          <strong>BOLD</strong>
+        </p>
+        <p>
+          <u>UNDERLINE</u>
+        </p>
+        <p>
+          <code>inline-code</code>
+        </p>
+        <pre class="ql-syntax" spellcheck="false">code-block</pre>
+        <p><br></p>
+        <ol>
+          <li>ordered</li>
+          <li>ordered</li>
+          <li>ordered</li>
+        </ol>
+        <p><br></p>
+        <ul>
+          <li>unordered</li>
+          <li>unordered</li>
+          <li>unordered</li>
+        </ul>
+        <p>
+          <br>
+        </p>
+        <p><a href="https://www.naver.com" rel="noopener noreferrer" target="_blank">Link</a></p><p><br></p><h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3><p>Normal</p>`,
     commentCount: faker.number.int(numberRange),
     likeCount: faker.number.int(numberRange),
     scrapCount: faker.number.int(numberRange),
