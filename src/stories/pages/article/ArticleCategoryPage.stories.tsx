ae40023a6ed18f1eaf983dca39354844c46bd778
Cola <bcad1591@gmail.com>
2023-08-04 18:54:01 +0900

    feat: Success 스토리 decorators 추가

--- src/stories/pages/article/ArticleCategoryPage.stories.tsx
+++ src/stories/pages/article/ArticleCategoryPage.stories.tsx
@@ -1,7 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
+
 import { getArticleCategories, getArticlesError } from '~/mocks/handlers';
 import ArticleCategoryPage from '~/pages/articles/categories/[categoryId]';
+import { queryKeys } from '~/react-query/common';
 import { PageLayout } from '~/stories/Layout';
 
 const meta: Meta<typeof ArticleCategoryPage> = {
@@ -25,10 +29,22 @@ type ArticleCategoryPageStory = StoryObj<typeof ArticleCategoryPage>;
 
 const successCaseCategoryId = 1;
 
-export const Default: ArticleCategoryPageStory = {
+export const Success: ArticleCategoryPageStory = {
   args: {
     categoryId: successCaseCategoryId,
   },
+  decorators: [
+    (Story) => {
+      const queryClient = useQueryClient();
+      useEffect(() => {
+        queryClient.resetQueries(
+          queryKeys.articles.list(successCaseCategoryId)
+        );
+      }, [queryClient]);
+
+      return <Story />;
+    },
+  ],
 };
 
 const errorCaseCategoryId = 2;
