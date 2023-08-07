29bbfa76ea1c159f1570f8f3faaaee5e51a7c46e
Cola <bcad1591@gmail.com>
2023-08-08 05:24:28 +0900

    refactor: handlePressedChange named function 추가, 변수 이름 변경
    - onPressedChange의 매개변수 이름이 다른 변수 이름과 중복되서(selectedOrder) pressed로 변경

--- src/components/Forms/PortfolioForm/Fields/Skills/SkillOption.tsx
+++ src/components/Forms/PortfolioForm/Fields/Skills/SkillOption.tsx
@@ -17,24 +17,25 @@ interface SkillSelectButtonProps {
 
 const SkillOption = (props: SkillSelectButtonProps) => {
   const { skillName, selectedOrderRef } = props;
+  const optionFieldName = `${fieldName}.${skillName}` as const;
   const { setCenter } = useTransformContext();
   const { setValue } = usePortfolioFormContext();
   const selectedOrder = useWatch<PortfolioFormValues>({
-    name: `${fieldName}.${skillName}`,
-    defaultValue: undefined,
+    name: optionFieldName,
   }) as number | undefined;
+  const getNextOrder = () => {
+    return selectedOrderRef.current++;
+  };
+  const handlePressedChange = (pressed: boolean) => {
+    setValue(optionFieldName, pressed ? getNextOrder() : undefined);
+    setTimeout(() => setCenter());
+  };
 
   return (
     <SkillBadge
       defaultPressed={!!selectedOrder}
       name={skillName}
-      onPressedChange={(selectedOrder) => {
-        setValue(
-          `${fieldName}.${skillName}`,
-          selectedOrder ? selectedOrderRef.current++ : undefined
-        );
-        setTimeout(() => setCenter());
-      }}
+      onPressedChange={handlePressedChange}
     />
   );
 };
