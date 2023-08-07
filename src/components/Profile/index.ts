d2db339905d5e38d73246777b4f244a879b517dd
Cola <bcad1591@gmail.com>
2023-08-07 19:37:12 +0900

    feat: ProfileTabsTriggers 추가

--- src/components/Profile/index.ts
+++ src/components/Profile/index.ts
@@ -1,9 +1,13 @@
 import MyInfoSettingsLink from './MyInfoSettingsLink';
 import PrivateProfileIndicator from './PrivateProfileIndicator';
 import NavItem from './ProfileNavItem';
+import TabsTriggers from './ProfileTabsTriggers';
 
 export const Profile = {
   NavItem,
   MyInfoSettingsLink,
   PrivateProfileIndicator,
+  TabsTriggers,
 };
+
+export * from './utils';
