ebd078e3e83dfe20ec479878cc27dfde943a3d8e
Cola <bcad1591@gmail.com>
2023-08-09 08:26:47 +0900

    refactor: DatePicker, Alert, BottomMenu dynamic 적용

--- src/components/GlobalModal/modals.ts
+++ src/components/GlobalModal/modals.ts
@@ -1,4 +1,18 @@
-import { Alert, BottomMenu } from '~/components/ModalContent';
+import dynamic from 'next/dynamic';
+
+const Alert = dynamic(
+  () => import('~/components/ModalContent').then((mod) => mod.Alert),
+  {
+    ssr: false,
+  }
+);
+
+const BottomMenu = dynamic(
+  () => import('~/components/ModalContent').then((mod) => mod.BottomMenu),
+  {
+    ssr: false,
+  }
+);
 
 export const modals = {
   alert: Alert,
