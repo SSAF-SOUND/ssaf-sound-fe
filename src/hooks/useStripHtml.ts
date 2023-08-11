78cc2948d68ca9a5467cff0ae6012e3244e8daa8
Cola <bcad1591@gmail.com>
2023-08-12 07:28:56 +0900

    feat: replaceMultipleSpacesWithSingle 추가, useStripHtml에 적용

--- src/hooks/useStripHtml.ts
+++ src/hooks/useStripHtml.ts
@@ -1,6 +1,11 @@
 import { useMemo } from 'react';
 import { stripHtml } from 'string-strip-html';
 
+import { replaceMultipleSpacesWithSingle } from '~/utils/replaceMultipleSpacesWithSingle';
+
 export const useStripHtml = (str: string) => {
-  return useMemo(() => stripHtml(str).result, [str]);
+  return useMemo(
+    () => replaceMultipleSpacesWithSingle(stripHtml(str).result),
+    [str]
+  );
 };
