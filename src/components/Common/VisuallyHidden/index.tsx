6e2cf1bcd3a4e2e4243c7683f1a845a57a5fced1
Cola <bcad1591@gmail.com>
2023-08-12 08:04:25 +0900

    feat: asChild 추가

--- src/components/Common/VisuallyHidden/index.tsx
+++ src/components/Common/VisuallyHidden/index.tsx
@@ -1,13 +1,19 @@
 import type { ReactNode } from 'react';
 
+import { Slot } from '@radix-ui/react-slot';
+
 import { visuallyHidden } from '~/styles/utils';
 
 interface VisuallyHiddenProps {
   children: ReactNode;
+  asChild?: boolean;
 }
 
 const VisuallyHidden = (props: VisuallyHiddenProps) => {
-  return <span css={visuallyHidden}>{props.children}</span>;
+  const { children, asChild = false } = props;
+  const Component = asChild ? Slot : 'span';
+
+  return <Component css={visuallyHidden}>{children}</Component>;
 };
 
 export default VisuallyHidden;
