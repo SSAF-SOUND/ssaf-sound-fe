e8affb01cd858d2a1aa43a5f19b9b8ba9239e1f2
Cola <bcad1591@gmail.com>
2023-08-14 08:07:26 +0900

    feat: 대댓글 작성, 댓글 수정, 댓글 삭제 추가

--- src/components/ArticleComment/index.tsx
+++ src/components/ArticleComment/index.tsx
@@ -1,7 +1,9 @@
+import type { ArticleCommentFormProps } from '~/components/Forms/ArticleCommentForm';
 import type {
   CommentDetail,
   CommentDetailWithoutReplies,
 } from '~/services/articleComment';
+import type { AnyFunction } from '~/types';
 
 import { css } from '@emotion/react';
 import { memo } from 'react';
@@ -12,10 +14,18 @@ import MoreButton from '~/components/ArticleComment/MoreButton';
 import ReplyButton from '~/components/ArticleComment/ReplyButton';
 import { Icon, Separator } from '~/components/Common';
 import Name from '~/components/Name';
+import {
+  useInvalidateArticleComments,
+  useRemoveArticleComment,
+  useReplyArticleComment,
+  useUpdateArticleComment,
+} from '~/services/articleComment';
 import { useMyInfo } from '~/services/member';
 import { populateDefaultUserInfo } from '~/services/member/utils/popoulateDefaultUserInfo';
 import { flex, fontCss, palettes } from '~/styles/utils';
-import { formatDateTime } from '~/utils';
+import { formatDateTime, handleAxiosError } from '~/utils';
+
+import { useArticleCommentMenu, useArticleCommentModalForm } from './utils';
 
 interface ArticleCommentProps {
   articleId: number;
@@ -26,6 +36,13 @@ interface ArticleCommentProps {
 const ArticleComment = memo((props: ArticleCommentProps) => {
   const { articleId, comment, leaf = false } = props;
   const { data: myInfo } = useMyInfo();
+  const {
+    isOpen: isArticleCommentModalFormOpen,
+    openArticleCommentModalForm,
+    closeArticleCommentModalForm,
+  } = useArticleCommentModalForm();
+
+  const { openArticleCommentMenu } = useArticleCommentMenu();
 
   const {
     author,
@@ -38,54 +55,144 @@ const ArticleComment = memo((props: ArticleCommentProps) => {
     createdAt,
     replies,
     modified,
+    deletedComment,
   } = comment;
 
+  const invalidateArticleComments = useInvalidateArticleComments(articleId);
+
+  const { mutateAsync: replyArticleComment } = useReplyArticleComment({
+    commentId,
+    articleId,
+  });
+
+  const {
+    mutateAsync: updateArticleComment,
+    isLoading: isUpdatingArticleComment,
+  } = useUpdateArticleComment(commentId);
+
+  const {
+    mutateAsync: removeArticleComment,
+    isLoading: isRemovingArticleComment,
+  } = useRemoveArticleComment(commentId);
+
+  const isLoadingMoreButton =
+    isUpdatingArticleComment || isRemovingArticleComment;
   const isSignedIn = !!myInfo;
-
   const userInfo = populateDefaultUserInfo(author);
-
   const { date, time } = formatDateTime(createdAt);
-
   const hasReplies = replies && replies.length > 0;
-
   const showReplyButton = isSignedIn && !leaf;
+
+  const handleCommentRequest = async (
+    callback: AnyFunction,
+    modalCloseBeforeCallbackInvoked = false
+  ) => {
+    try {
+      if (modalCloseBeforeCallbackInvoked) {
+        closeArticleCommentModalForm();
+        await callback();
+      } else {
+        await callback();
+        closeArticleCommentModalForm();
+      }
+      invalidateArticleComments();
+    } catch (err) {
+      handleAxiosError(err);
+    }
+  };
+
+  const handleCreateCommentReply: ArticleCommentFormProps['onValidSubmit'] =
+    async (_, formValues) => {
+      await handleCommentRequest(() => replyArticleComment(formValues));
+    };
+
+  const handleEditComment: ArticleCommentFormProps['onValidSubmit'] = async (
+    _,
+    formValues
+  ) => {
+    await handleCommentRequest(() => updateArticleComment(formValues));
+  };
+
+  const handleRemoveComment = () => {
+    handleCommentRequest(removeArticleComment, true);
+  };
+
+  const onClickReplyButton = () => {
+    openArticleCommentModalForm({ onValidSubmit: handleCreateCommentReply });
+  };
+
+  const onClickMoreButton = () => {
+    openArticleCommentMenu({
+      mine,
+      onClickEdit: () =>
+        openArticleCommentModalForm({
+          defaultValues: { content, anonymous: anonymity },
+          onValidSubmit: handleEditComment,
+        }),
+      onClickRemoveAction: handleRemoveComment,
+      onClickReport: () => {
+        console.log('신고페이지로');
+      },
+    });
+  };
 
   return (
     <div css={selfCss}>
-      <div css={[commentLayerCss, leaf && leafCss]}>
-        <header css={headerCss}>
-          {/* eslint-disable-next-line */}
-          {/* @ts-ignore */}
-          <Name userInfo={userInfo} size="sm" />
+      <div
+        css={[
+          commentLayerCss,
+          leaf && leafCss,
+          isArticleCommentModalFormOpen && selfHighLightCss,
+        ]}
+      >
+        {deletedComment && (
+          <span css={deletedCommentCss}>삭제된 댓글입니다.</span>
+        )}
 
-          <div css={buttonLayerCss}>
-            {showReplyButton && <ReplyButton />}
-            <LikeLayer
-              liked={liked}
-              likeCount={likeCount}
-              articleId={articleId}
-              commentId={commentId}
-              isSignedIn={isSignedIn}
-            />
-            {isSignedIn && <MoreButton />}
-          </div>
-        </header>
+        {!deletedComment && (
+          <>
+            <header css={headerCss}>
+              {/* eslint-disable-next-line */}
+              {/* @ts-ignore */}
+              <Name userInfo={userInfo} size="sm" />
 
-        <div css={{ marginBottom: 4 }}>
-          <CommentContent content={content} />
-          {modified && <span css={modifiedCss}>(수정됨)</span>}
-        </div>
+              <div css={buttonLayerCss}>
+                {showReplyButton && (
+                  <ReplyButton onClick={onClickReplyButton} />
+                )}
+                <LikeLayer
+                  liked={liked}
+                  likeCount={likeCount}
+                  articleId={articleId}
+                  commentId={commentId}
+                  isSignedIn={isSignedIn}
+                />
+                {isSignedIn && (
+                  <MoreButton
+                    onClick={onClickMoreButton}
+                    loading={isLoadingMoreButton}
+                  />
+                )}
+              </div>
+            </header>
 
-        <div css={dateTimeCss}>
-          <span>{date}</span>
-          <Separator
-            orientation="vertical"
-            backgroundColor={palettes.font.blueGrey}
-            css={{ margin: '0 8px' }}
-            height={12}
-          />
-          <span>{time}</span>
-        </div>
+            <div css={{ marginBottom: 4 }}>
+              <CommentContent content={content} />
+              {modified && <span css={modifiedCss}>(수정됨)</span>}
+            </div>
+
+            <div css={dateTimeCss}>
+              <span>{date}</span>
+              <Separator
+                orientation="vertical"
+                backgroundColor={palettes.font.blueGrey}
+                css={{ margin: '0 8px' }}
+                height={12}
+              />
+              <span>{time}</span>
+            </div>
+          </>
+        )}
       </div>
 
       {hasReplies && (
@@ -141,3 +248,9 @@ const replyCss = css(
 );
 
 const modifiedCss = css({ color: palettes.primary.default }, fontCss.style.R14);
+
+const selfHighLightCss = css({
+  background: 'red',
+});
+
+const deletedCommentCss = css(fontCss.style.R14);
