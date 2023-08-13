ce9e0f9437777a0389e6fb89dfe6c33761b95b20
otterp012 <otterp012@gmail.com>
2023-08-13 18:56:13 +0900

    chore: 필요없는 내용 제거

--- src/utils/getBlurImage.ts
+++ src/utils/getBlurImage.ts
@@ -12,4 +12,5 @@ export const getAllBlurImages = async (imageUrlArray: string[]) => {
   return blurImages.map((blurImage) => blurImage.base64);
 };
 
-const T = () => false;
+// re-export 사용시 child-process 관련 오류가 일어나는데,
+// 해당 방법의 수정 사항이 package.json을 건드는 방법이라 우선 건너갑니다!
