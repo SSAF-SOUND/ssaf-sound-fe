7a43e9b0d25032d5586e8d6e3e65192cdb372cc2
otterp012 <otterp012@gmail.com>
2023-08-15 06:41:59 +0900

    feat: queryKey 추가

--- src/react-query/common/queryKeys.ts
+++ src/react-query/common/queryKeys.ts
@@ -27,7 +27,23 @@ export const queryKeys = {
   comments: {
     list: (articleId: number) => ['comments', articleId],
   },
+
+  recruit: {
+    list: () => ['recruits'],
+    detail: (recruitId: number) => ['recruits', recruitId],
+    members: (recruitId: number) => ['recruits', 'members', recruitId],
+    scrap: (recruitId: number) => ['recruits', 'scrap', recruitId],
+  },
+  lunch: {
+    menus: ({ campus, date }: any) => ['lunch', 'menus', campus, date],
+    detail: (lunchId: number) => ['lunch', lunchId],
+  },
 };
+// menus: ({ campus, date }: any) =>
+// `/lunch?campus=${campus}&date=${date}` as const,
+// detail: (lunchId: number) => `/lunch/${lunchId}` as const,
+// vote: (lunchId: number) => `/lunch/poll/${lunchId}` as const,
+// revertVote: (lunchId: number) => `/lunch/poll/revert/${lunchId}` as const,
 
 export const endpoints = {
   auth: {
@@ -121,12 +137,23 @@ export const endpoints = {
   },
   recruit: {
     // todo 이름, 파라미터 수정
-    data: () => '/recruits' as const,
-    members: (recruitId: string) => `/recruits/${recruitId}/members` as const,
-    detail: (recruitId: string) => `/recruits/${recruitId}/detail` as const,
+    list: () => '/recruits' as const,
+    members: (recruitId: number) => `/recruits/${recruitId}/members` as const,
+    detail: (recruitId: number) => `/recruits/${recruitId}/detail` as const,
+    scrap: (recruitId: number) => `/recruits/${recruitId}/scrap` as const,
   },
   meta: {
     campuses: () => '/meta/campuses' as const,
     skills: () => '/meta/skills' as const,
+  },
+  lunch: {
+    menus: ({ campus, date }: any) => {
+      const queryString = new URLSearchParams({ campus, date }).toString();
+      return `/lunch?${queryString}`;
+    },
+    detail: (lunchId: number) => `/lunch/${lunchId}` as const,
+    vote: (lunchId: number) => `/lunch/poll/${lunchId}` as const,
+    revertVote: (lunchId: number) => `/lunch/poll/revert/${lunchId}` as const,
+    error: () => `/lunch/error` as const,
   },
 };
