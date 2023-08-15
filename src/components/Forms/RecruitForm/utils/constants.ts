f38b6a3eaac245d0b194047f38570686130d637f
Cola <bcad1591@gmail.com>
2023-08-16 07:50:12 +0900

    feat: populateDefaultValues 삭제, constants 추가

--- src/components/Forms/RecruitForm/utils/constants.ts
+++ src/components/Forms/RecruitForm/utils/constants.ts
@@ -1,2 +1,14 @@
 // meta
-export const parts = ['프론트엔드', '백엔드', '기획/디자인', '앱'];
+
+export enum RecruitParts {
+  FRONTEND = '프론트엔드',
+  BACKEND = '백엔드',
+  PM = '기획/디자인',
+  APP = '앱',
+  STUDY = '스터디',
+}
+
+export enum RecruitCategoryName {
+  PROJECT = '프로젝트',
+  STUDY = '스터디',
+}
