38f1d4e6ceed84a44099cab17bcbd79a19fcd792
Cola <bcad1591@gmail.com>
2023-08-17 02:09:24 +0900

    feat: 유효성 검사와 에러 메세지 업데이트

--- src/components/Forms/RecruitForm/Fields/Participants/StudyParticipants/index.tsx
+++ src/components/Forms/RecruitForm/Fields/Participants/StudyParticipants/index.tsx
@@ -8,36 +8,56 @@ import { useWatch } from 'react-hook-form';
 
 import { NumberInput } from '~/components/Common';
 import FieldOverview from '~/components/Forms/RecruitForm/Common/FieldOverview';
-import { useRecruitFormContext } from '~/components/Forms/RecruitForm/utils';
+import {
+  maxParticipantsCount,
+  minParticipantsCount,
+  RecruitCategoryName,
+  useRecruitFormContext,
+} from '~/components/Forms/RecruitForm/utils';
 import { createBoundClamp } from '~/utils';
 
-const minCount = 1;
-const maxCount = 20;
 const fieldArrayName = 'participants.study';
-const clamp = createBoundClamp([minCount, maxCount]);
-
-const validateParticipantsCount = (value: number) => {
-  if (value > 20 || value < 1)
-    return '모집 인원은 1명 ~ 20명 사이만 가능합니다.';
-};
+const participantsFieldName = `${fieldArrayName}.0` as const;
+const clamp = createBoundClamp([minParticipantsCount, maxParticipantsCount]);
 
 interface StudyParticipantsProps {
   className?: string;
 }
 
+const validateStudyParticipants = (
+  { count }: RecruitParticipants,
+  formValues: RecruitFormValues
+) => {
+  if (formValues.category !== RecruitCategoryName.STUDY) {
+    return true;
+  }
+
+  if (count < minParticipantsCount || count > maxParticipantsCount) {
+    return `모집 인원은 ${minParticipantsCount}명 ~ ${maxParticipantsCount}명 사이만 가능합니다.`;
+  }
+
+  return true;
+};
+
 const StudyParticipants = (props: StudyParticipantsProps) => {
   const { className } = props;
   const { register, setValue } = useRecruitFormContext();
   const participants = useWatch<RecruitFormValues>({
-    name: `${fieldArrayName}.0`,
+    name: participantsFieldName,
   }) as RecruitParticipants;
   const { count } = participants;
 
   const countFieldName = `${fieldArrayName}.0.count` as const;
 
   const handleChangeCount = (amount: number) => () => {
-    setValue(countFieldName, clamp(count + amount));
+    setValue(countFieldName, clamp(count + amount), {
+      shouldDirty: true,
+    });
   };
+
+  register(participantsFieldName, {
+    validate: validateStudyParticipants,
+  });
 
   return (
     <div className={className}>
@@ -45,12 +65,11 @@ const StudyParticipants = (props: StudyParticipantsProps) => {
 
       <NumberInput
         css={numberInputCss}
-        min={minCount}
-        max={maxCount}
+        min={minParticipantsCount}
+        max={maxParticipantsCount}
         onClickMinus={handleChangeCount(-1)}
         onClickPlus={handleChangeCount(1)}
         {...register(countFieldName, {
-          validate: validateParticipantsCount,
           valueAsNumber: true,
         })}
       />
