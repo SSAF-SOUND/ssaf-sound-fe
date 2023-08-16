1fcc0474d7097bf070d7ed7ed31ea9c5fe42ad87
Cola <bcad1591@gmail.com>
2023-08-16 09:01:14 +0900

    feat: 선택 필드 마크업, 스타일 업데이트

--- src/components/Forms/RecruitForm/Fields/Contact/index.tsx
+++ src/components/Forms/RecruitForm/Fields/Contact/index.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/react';
 
 import { TextInput } from '~/components/Common';
 import { useRecruitFormContext } from '~/components/Forms/RecruitForm/utils';
+import { palettes } from '~/styles/utils';
 
 const fieldName = 'contact';
 const maxLength = 500;
@@ -10,6 +11,7 @@ const validateContact = (value: string) => {
   if (value.length > maxLength) {
     return `연락처의 길이는 ${maxLength}자 이하여야 합니다.`;
   }
+  return true;
 };
 
 interface ContactProps {
@@ -25,6 +27,10 @@ export const Contact = (props: ContactProps) => {
 
   return (
     <div className={className}>
+      <div css={titleCss}>
+        오픈카톡방 또는 디스코드 등 연락할 수 있는 링크를 남겨주세요 (선택)
+      </div>
+
       <TextInput
         size="md"
         placeholder="링크를 입력해주세요"
@@ -40,4 +46,10 @@ export const Contact = (props: ContactProps) => {
 
 const inputCss = css({
   width: '100%',
+});
+
+const titleCss = css({
+  padding: '12px 25px',
+  backgroundColor: palettes.background.grey,
+  marginBottom: 32,
 });
--- src/components/Forms/RecruitForm/Fields/QuestionToApplicants/index.tsx
+++ src/components/Forms/RecruitForm/Fields/QuestionToApplicants/index.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/react';
 
 import { TextInput } from '~/components/Common';
 import { useRecruitFormContext } from '~/components/Forms/RecruitForm/utils';
+import { fontCss, palettes } from '~/styles/utils';
 
 const fieldName = 'questionToApplicants';
 const maxLength = 50;
@@ -9,6 +10,7 @@ const validateQuestionToApplicants = (value: string) => {
   if (value.length > maxLength) {
     return `질문은 ${maxLength}자 이하로 작성할 수 있습니다.`;
   }
+  return true;
 };
 
 interface QuestionToApplicantsProps {
@@ -28,6 +30,14 @@ export const QuestionToApplicants = (props: QuestionToApplicantsProps) => {
 
   return (
     <div className={className}>
+      <div css={titleCss}>
+        <strong css={{ color: palettes.primary.default }}>[등록자 질문]</strong>{' '}
+        <span>신청자에게 꼭 물어보고 싶은 질문을 적어주세요 (선택)</span>
+      </div>
+
+      <p css={descriptionCss}>
+        {`질문을 작성하여 리쿠르팅 참여자에게 답변을 받을 수 있습니다. (글자 수 ${maxLength}자 내)`}
+      </p>
       {readonly ? (
         <TextInput
           css={inputCss}
@@ -50,6 +60,17 @@ export const QuestionToApplicants = (props: QuestionToApplicantsProps) => {
     </div>
   );
 };
+
+const titleCss = css({
+  padding: '12px 25px',
+  backgroundColor: palettes.background.grey,
+  marginBottom: 10,
+});
+
+const descriptionCss = css(
+  { color: palettes.primary.light, cursor: 'pointer', marginBottom: 10 },
+  fontCss.style.R12
+);
 
 const inputCss = css({
   width: '100%',
