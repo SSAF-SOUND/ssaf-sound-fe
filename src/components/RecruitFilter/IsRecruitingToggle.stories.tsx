bbbd951ad1eddbd53d0127722b1082205f376a7c
otterp012 <otterp012@gmail.com>
2023-08-20 12:45:26 +0900

    feat: 스토리 추가

---
+++ src/components/RecruitFilter/IsRecruitingToggle.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta } from '@storybook/react';
+import { IsRecruitingToggle } from './IsRecruitingToggle';
+
+const meta: Meta<typeof IsRecruitingToggle> = {
+  title: 'Recruit/IsRecruitingToggle',
+  component: IsRecruitingToggle,
+  argTypes: {},
+};
+
+export default meta;
+
+export const Default = () => {
+  return <IsRecruitingToggle />;
+};
