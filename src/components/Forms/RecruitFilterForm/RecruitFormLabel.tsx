5793e676bf2d99c02783a4cff70722bbadc19572
otterp012 <otterp012@gmail.com>
2023-08-21 02:19:32 +0900

    feat: RecruitFilterFormLabel

---
+++ src/components/Forms/RecruitFilterForm/RecruitFormLabel.tsx
@@ -0,0 +1,22 @@
+import { css } from '@emotion/react';
+import { Separator } from '~/components/Common';
+import { flex, fontCss, palettes } from '~/styles/utils';
+
+export const RecruitFormLabel = () => {
+  return (
+    <div css={selfCss}>
+      <Separator
+        orientation="horizontal"
+        width="12%"
+        height="5px"
+        css={{
+          background: palettes.font.blueGrey,
+        }}
+      />
+      <label css={labelCss}>상세 옵션 선택</label>
+    </div>
+  );
+};
+
+const selfCss = css(flex('center', '', 'column', 6));
+const labelCss = css(fontCss.family.auto, fontCss.style.R12);
