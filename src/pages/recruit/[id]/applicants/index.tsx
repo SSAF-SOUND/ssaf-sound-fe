ff135efdc3785962adc3c1caa464ba3bdf5f5bf6
Cola <bcad1591@gmail.com>
2023-08-24 07:54:07 +0900

    feat: 리쿠르팅 신청목록 페이지

--- src/pages/recruit/[id]/applicants/index.tsx
+++ src/pages/recruit/[id]/applicants/index.tsx
@@ -1,79 +1,150 @@
+import type {
+  CustomNextPage,
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+} from 'next/types';
+import type { RecruitParts } from '~/services/recruit';
+
+import { useRouter } from 'next/router';
+
 import { css } from '@emotion/react';
 
-import { Avatar, Toggle } from '~/components/Common';
-import { RecruitLayout } from '~/components/Layout';
-import { RecruitApplicantBar } from '~/components/RecruitApplicants';
+import { DefaultFullPageLoader, loaderText } from '~/components/Common';
+import {
+  RecruitApplicantsDetail,
+  RecruitApplicantsAccordion,
+} from '~/components/RecruitApplicants';
 import TitleBar from '~/components/TitleBar';
-import { useGetQueryString } from '~/hooks';
-import { flex, fontCss, palettes } from '~/styles/utils';
+import { useRecruitApplicants, useRecruitDetail } from '~/services/recruit';
+import { flex, fontCss, palettes, titleBarHeight } from '~/styles/utils';
 import { routes } from '~/utils';
 
-const a = css(fontCss.family.auto, fontCss.style.B28, {
-  color: palettes.white,
-});
-
-const b = css(fontCss.family.auto, fontCss.style.R12, {
-  color: palettes.recruit.default,
-});
-
-const recruitIdQueryKey = 'id';
+interface RecruitApplicantsPageProps
+  extends InferGetServerSidePropsType<typeof getServerSideProps> {}
 
 /**
  * NOTE: 내 리쿠르트인 경우에만 접근 가능한 페이지 (서버에서 검증)
  */
-const RecruitApplicantsPage = () => {
-  const recruitId = useGetQueryString(recruitIdQueryKey);
-  const backwardRoute = recruitId && routes.recruit.detail(Number(recruitId));
+const RecruitApplicantsPage: CustomNextPage<RecruitApplicantsPageProps> = (
+  props
+) => {
+  const { recruitId } = props;
+  const router = useRouter();
+
+  const {
+    data: recruitApplicants,
+    isLoading: isRecruitApplicantsLoading,
+    isError: isRecruitApplicantsError,
+    error: recruitApplicantsError,
+  } = useRecruitApplicants(recruitId);
+
+  const {
+    data: recruitDetail,
+    isLoading: isRecruitDetailLoading,
+    isError: isRecruitDetailError,
+    error: recruitDetailError,
+  } = useRecruitDetail(recruitId);
+
+  if (isRecruitApplicantsLoading || isRecruitDetailLoading) {
+    return <DefaultFullPageLoader text={loaderText.loadingData} />;
+  }
+
+  if (isRecruitApplicantsError || isRecruitDetailError) {
+    // NOTE: unauthorized error인 경우 redirect, 그 외에는 데이터 로딩 실패 페이지
+    const isUnauthorized = true as boolean;
+
+    if (isUnauthorized) {
+      router.replace(routes.unauthorized());
+      return <DefaultFullPageLoader />;
+    }
+
+    return <div>Fail to load</div>;
+  }
+
+  const partAndApplicantsEntries = Object.entries(
+    recruitApplicants.recruitApplications
+  );
 
   return (
-    <RecruitLayout>
+    <div css={selfCss}>
       <TitleBar.Default
         title="리쿠르팅 신청 목록"
         withoutClose
-        onClickBackward={backwardRoute}
+        onClickBackward={routes.recruit.detail(recruitId)}
       />
 
-      <div>
-        <div css={flex('', '', 'column', 0)}>
-          <span css={a}>기획/디자인</span>
-          <span css={b}>6명 중 2명 모집 완료</span>
-        </div>
-
-        <div css={flex('', '', 'row', 6)}>
-          <Avatar size="lg" userInfo={{ isMajor: true, nickname: 'Test' }} />
-          <Avatar size="lg" userInfo={{ isMajor: false, nickname: 'A' }} />
-          <Avatar size="lg" userInfo={{ isMajor: false, nickname: 'B' }} />
-          <Avatar size="lg" userInfo={{ isMajor: false, nickname: 'C' }} />
-          <Avatar size="lg" userInfo={{ isMajor: false, nickname: 'D' }} />
-          <Avatar size="lg" userInfo={{ isMajor: true, nickname: 'E' }} />
-          <Avatar size="lg" />
-        </div>
-
-        <div css={flex('', '', 'column', 20)}>
-          <div css={flex('center', 'space-between', 'row', 6)}>
-            <span css={[fontCss.family.auto, fontCss.style.B18]}>
-              리쿠르팅 신청
-              <span css={{ color: palettes.recruit.default }}> 9</span>
-            </span>
-            <Toggle
-              thumbSize={20}
-              textWidth={40}
-              text="하트"
-              theme="recruit"
-              padding={2}
-            />
-          </div>
-          <ol>
-            <RecruitApplicantBar withHeart />
-            <RecruitApplicantBar withHeart />
-            <RecruitApplicantBar withHeart />
-            <RecruitApplicantBar withHeart />
-            <RecruitApplicantBar withHeart />
-          </ol>
-        </div>
+      <div css={headerCss}>
+        <p css={categoryNameCss}>{recruitDetail.category}</p>
+        <p css={titleCss}>{recruitDetail.title}</p>
       </div>
-    </RecruitLayout>
+
+      <RecruitApplicantsAccordion.Root>
+        {partAndApplicantsEntries.map(([part, applicants]) => (
+          <RecruitApplicantsDetail
+            key={part}
+            part={part as RecruitParts}
+            applicants={applicants}
+            recruitDetail={recruitDetail}
+          />
+        ))}
+      </RecruitApplicantsAccordion.Root>
+    </div>
   );
 };
 
+const selfPaddingY = titleBarHeight + 10;
+const selfCss = css({
+  padding: `${selfPaddingY}px 15px`,
+});
+
+const headerCss = css(
+  {
+    padding: '0 5px',
+    marginBottom: 16,
+    color: palettes.font.blueGrey,
+    wordBreak: 'break-word',
+  },
+  fontCss.style.R14,
+  flex('center', '', 'row', 16)
+);
+
+const categoryNameCss = css({
+  flexShrink: 0,
+});
+
+const titleCss = css({
+  color: palettes.white,
+});
+
 export default RecruitApplicantsPage;
+RecruitApplicantsPage.auth = {
+  role: 'user',
+  loading: <DefaultFullPageLoader text={loaderText.checkUser} />,
+  unauthorized: routes.unauthorized(),
+};
+
+interface Props {
+  recruitId: number;
+}
+
+type Params = {
+  id: string;
+};
+
+export const getServerSideProps: GetServerSideProps<Props, Params> = async (
+  context
+) => {
+  const id = Number(context.params?.id);
+
+  if (Number.isNaN(id)) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      recruitId: id,
+    },
+  };
+};
