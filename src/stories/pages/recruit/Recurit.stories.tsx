185d0f21cd1c8c8ce34daf11ba577947ac369d30
otterp012 <otterp012@gmail.com>
2023-08-26 03:58:14 +0900

    feat: RecruitPage Story 추가

---
+++ src/stories/pages/recruit/Recurit.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import RecruitPage from '~/pages/recruit';
+import { PageLayout } from '~/stories/Layout';
+
+const meta: Meta<typeof RecruitPage> = {
+  title: 'Page/Recruit',
+  component: RecruitPage,
+  decorators: [
+    (Story) => (
+      <PageLayout>
+        <Story />
+      </PageLayout>
+    ),
+  ],
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+
+type RecruitApplyPageStory = StoryObj<typeof RecruitPage>;
+
+export const Default: RecruitApplyPageStory = {};
