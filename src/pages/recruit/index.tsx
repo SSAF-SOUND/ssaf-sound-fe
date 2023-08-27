f27acc46a9b4cb4a8464eb418a400fa607fba54c
otterp012 <otterp012@gmail.com>
2023-08-28 02:36:36 +0900

    feat: Recruit Page

--- src/pages/recruit/index.tsx
+++ src/pages/recruit/index.tsx
@@ -1,42 +1,127 @@
-import type { GetServerSideProps } from 'next/types';
+import type { GetServerSideProps } from 'next';
+import type { RecruitCategory } from '~/services/recruit';
 
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-const Recruit = () => {
+import { QueryClient } from '@tanstack/react-query';
+
+import ErrorCard from '~/components/ErrorCard';
+import { RecruitSearchForm } from '~/components/Forms/RecruitSearchForm';
+import { RecruitLayout } from '~/components/Layout';
+import { RecruitCards } from '~/components/RecruitCard';
+import { RecruitCreateButton } from '~/components/RecruitCreateLink';
+import {
+  IsRecruitingToggle,
+  RecruitFilterModal,
+  RecruitFilterTabs,
+} from '~/components/RecruitFilter';
+import TopBar from '~/components/TopBar';
+import { useGetQueryString } from '~/hooks';
+import { queryKeys } from '~/react-query/common';
+import { dehydrate } from '~/react-query/server';
+import { useInfiniteRecruits, getRecruits } from '~/services/recruit';
+import { recruitTypeConvertor } from '~/services/recruit/utils/recruitTypeConvertor';
+import { flex } from '~/styles/utils';
+
+const Recruit = (props: { IS_SSR_ERROR: boolean }) => {
+  const categoryQuery = useGetQueryString('category');
+  const category = (categoryQuery ?? 'project') as RecruitCategory;
+  const router = useRouter();
+
+  const isSSRError = props.IS_SSR_ERROR;
+  const { isLoading, refetch } = useInfiniteRecruits(
+    recruitTypeConvertor(router.query)
+  );
+
   return (
-    <div>
-      <Link
-        href={{
-          pathname: 'recruit/1',
-        }}
+    <RecruitLayout>
+      <TopBar />
+
+      <div
+        css={[
+          {
+            marginBottom: 20,
+          },
+          // fixTopCenter,
+          // {
+          //   top: 50,
+          //   zIndex: zIndex.fixed.normal,
+          //   background: palettes.background.default,
+          //   padding: '20px',
+          // },
+        ]}
       >
-        id = 1
-      </Link>
-      <div>---------------</div>
-      <Link
-        href={{
-          pathname: 'recruit/2',
-        }}
-      >
-        id = 2
-      </Link>
-    </div>
+        <div css={{ height: 20 }} />
+        <RecruitSearchForm />
+        <div css={{ height: 20 }} />
+        <div
+        // css={[
+        //   fixedFullWidth,
+        //   {
+        //     marginLeft: '-20px',
+        //   },
+        // ]}
+        >
+          <RecruitFilterTabs />
+        </div>
+
+        <div css={{ height: 10 }} />
+        <div css={[flex('center', 'space-between', 'row')]}>
+          <div css={flex('', '', 'row', 6)}>
+            <IsRecruitingToggle />
+            <RecruitFilterModal category={category} />
+          </div>
+          <RecruitCreateButton category={category} />
+        </div>
+      </div>
+      {!isSSRError && <RecruitCards />}
+      {isSSRError && (
+        <ErrorCard
+          buttonText="새로고침 하기"
+          onClickRetry={refetch}
+          isLoading={isLoading}
+        />
+      )}
+    </RecruitLayout>
   );
 };
 
 export default Recruit;
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  // const res = await fetch(`http://localhost/recruits`);
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const queryClient = new QueryClient();
+  const recruitsQueryKey = queryKeys.recruit.list({ ...context.query });
 
-  // // Only absolute URLs are supported
-  // const data: any = await res.json();
+  try {
+    await queryClient.fetchInfiniteQuery({
+      queryKey: recruitsQueryKey,
+      queryFn: (d) => {
+        return getRecruits({
+          recruits: recruitTypeConvertor(context.query),
+          cursor: d.pageParam,
+        });
+      },
+    });
 
-  // console.log(res);
-  // 추후에 prefetch 적용
-  return {
-    props: {
-      data: 1,
-    },
-  };
+    const { dehydratedState } = dehydrate(queryClient);
+    dehydratedState.queries.forEach((query) => {
+      // https://github.com/TanStack/query/issues/1458#issuecomment-1022396964
+      // eslint-disable-next-line
+      // @ts-ignore
+      if ('pageParams' in query.state.data) {
+        query.state.data.pageParams = [null];
+      }
+    });
+    return {
+      props: {
+        dehydratedState,
+      },
+    };
+  } catch {
+    return {
+      props: {
+        IS_SSR_ERROR: true,
+      },
+    };
+  }
 };
