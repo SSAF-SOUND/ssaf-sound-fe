3da8910e2ee294c9afdf04b3f66f4f4f003cc2a7
Cola <bcad1591@gmail.com>
2023-09-09 03:21:40 +0900

    feat: RecruitBasicInfo

---
+++ src/components/Recruit/Recruit/RecruitBasicInfo/RecruitSkillInfo.tsx
@@ -0,0 +1,23 @@
+import type { SkillName } from '~/services/recruit';
+
+import { css } from '@emotion/react';
+
+import { SkillIcon } from '~/components/Common';
+import { flex } from '~/styles/utils';
+
+interface RecruitSkillInfoProps {
+  skills: SkillName[];
+}
+
+export const RecruitSkillInfo = (props: RecruitSkillInfoProps) => {
+  const { skills } = props;
+  return (
+    <div css={selfCss}>
+      {skills.map((skill) => (
+        <SkillIcon key={skill} name={skill} />
+      ))}
+    </div>
+  );
+};
+
+const selfCss = css(flex('center', 'flex-start', 'row', 4, 'wrap'));
