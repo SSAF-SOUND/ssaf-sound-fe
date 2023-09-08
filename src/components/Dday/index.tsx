374e44bb565fa5195764dde21085c38de2f8d6d8
Cola <bcad1591@gmail.com>
2023-09-08 18:13:59 +0900

    refactor: Dday

--- src/components/Dday/index.tsx
+++ src/components/Dday/index.tsx
@@ -1,51 +1,44 @@
 import type { SerializedStyles } from '@emotion/react';
-import type { ComponentPropsWithoutRef } from 'react';
-import type { RecruitCategory } from '~/services/recruit';
+import type { RecruitCategoryName } from '~/services/recruit';
 
 import { css } from '@emotion/react';
 
 import { getRecruitThemeByCategory } from '~/services/recruit';
-import { fontCss, themeColorVars } from '~/styles/utils';
+import { fontCss, inlineFlex, themeColorVars } from '~/styles/utils';
 import { getDateDiff } from '~/utils';
 
-export interface DdayProps extends ComponentPropsWithoutRef<'span'> {
-  recruitEnd: string;
-  category: RecruitCategory;
+export interface DdayProps {
+  endDate: string;
+  category: RecruitCategoryName;
   size?: 'sm' | 'md';
 }
 
-type DdaySizes = 'sm' | 'md';
+export type DdaySize = 'sm' | 'md';
 
-const Dday = (props: DdayProps) => {
-  const {
-    recruitEnd = '2023-06-30',
-    category = 'project',
-    size = 'md',
-    ...restProps
-  } = props;
+export const Dday = (props: DdayProps) => {
+  const { endDate = '2023-06-30', category, size = 'sm' } = props;
 
-  const diff = getDateDiff(new Date(recruitEnd));
-  const isAbleDate = diff >= 0;
-  const dDay = isAbleDate ? `D - ${diff}` : '모집 완료';
+  const diff = getDateDiff(new Date(endDate));
+  const expired = diff > 0;
+  const dDay = expired ? `D - ${diff}` : '모집 완료';
 
   return (
     <span
-      css={[textCss, sizeCss[size]]}
+      css={[selfCss, textCss, sizeCss[size]]}
       data-theme={getRecruitThemeByCategory(category)}
-      {...restProps}
     >
       {dDay}
     </span>
   );
 };
 
+const selfCss = css(inlineFlex('center', 'center'));
+
 const textCss = css(fontCss.family.auto, {
-  color: themeColorVars.mainColor.var,
+  color: themeColorVars.mainDarkColor.var,
 });
 
-const sizeCss: Record<DdaySizes, SerializedStyles> = {
+const sizeCss: Record<DdaySize, SerializedStyles> = {
   sm: css(fontCss.style.B16),
   md: css(fontCss.style.B20),
 };
-
-export default Dday;
