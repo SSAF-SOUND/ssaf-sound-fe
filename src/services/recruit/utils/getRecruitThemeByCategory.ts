52c1910180689587a57fae3af01e2c315cff37de
Cola <bcad1591@gmail.com>
2023-09-08 18:09:36 +0900

    refactor: getRecruitThemeByCategory

--- src/services/recruit/utils/getRecruitThemeByCategory.ts
+++ src/services/recruit/utils/getRecruitThemeByCategory.ts
@@ -1,5 +1,14 @@
-import type { RecruitCategory } from './types';
+import { Theme } from '~/styles/utils';
 
-export const getRecruitThemeByCategory = (recruitType: RecruitCategory) => {
-  return recruitType === 'project' ? 'primary' : 'secondary';
+import { RecruitCategoryName } from './types';
+
+export const getRecruitThemeByCategory = (
+  category: RecruitCategoryName
+): Extract<Theme, Theme.PRIMARY | Theme.SECONDARY> => {
+  if (category === RecruitCategoryName.PROJECT) return Theme.PRIMARY;
+  if (category === RecruitCategoryName.STUDY) return Theme.SECONDARY;
+
+  throw new Error(
+    `잘못된 카테고리가 전달되었습니다. \n> category: ${category}`
+  );
 };
