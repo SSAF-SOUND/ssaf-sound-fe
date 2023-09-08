b643ea02cbdea50b80572c9e88015cec48956460
Cola <bcad1591@gmail.com>
2023-09-09 05:46:20 +0900

    feat: iconColor props 추가

--- src/components/Recruit/Recruit/RecruitIconButton.tsx
+++ src/components/Recruit/Recruit/RecruitIconButton.tsx
@@ -7,16 +7,27 @@ interface RecruitIconButtonProps {
   className?: string;
   onClick?: () => void;
   theme?: IconButtonProps['theme'];
+  iconColor?: string;
   label?: string;
 }
 
 export const RecruitIconButton = (props: RecruitIconButtonProps) => {
-  const { iconName, theme, label, ...restProps } = props;
+  const { iconName, iconColor, theme, label, ...restProps } = props;
+
   return (
     <IconButton {...restProps} theme={theme} size={iconButtonSize}>
-      <Icon name={iconName} size={iconSize} label={label} />
+      <Icon name={iconName} size={iconSize} label={label} color={iconColor} />
     </IconButton>
   );
+};
+
+interface RecruitIconProps
+  extends Pick<RecruitIconButtonProps, 'iconName' | 'className' | 'label'> {
+  color?: string;
+}
+export const RecruitIcon = (props: RecruitIconProps) => {
+  const { iconName } = props;
+  return <Icon name={iconName} {...props} />;
 };
 
 const iconSize = 24;
