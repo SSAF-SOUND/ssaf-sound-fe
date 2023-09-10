ebf69f7785ea1ef5aa92fdee851738dddd7e7259
Cola <bcad1591@gmail.com>
2023-09-10 09:10:03 +0900

    feat: 리쿠르팅 상세 페이지 Story

--- src/stories/pages/recruit/RecruitDetailPage.stories.tsx
+++ src/stories/pages/recruit/RecruitDetailPage.stories.tsx
@@ -1,12 +1,29 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import type { ComponentType } from 'react';
 
+import { useEffect } from 'react';
+
+import {
+  getRecruitDetail,
+  getRecruitParticipantsError,
+  removeRecruitError,
+  reportError,
+  scrapRecruitError,
+} from '~/mocks/handlers';
+import { userInfo } from '~/mocks/handlers/member/data';
 import RecruitDetailPage from '~/pages/recruits/[recruitId]';
+import { useSetMyInfo } from '~/services/member';
+import { useSetRecruitDetail } from '~/services/recruit';
 import { PageLayout } from '~/stories/Layout';
+import { disableArgs } from '~/stories/utils';
+
+const projectRecruitId = 0;
+const studyRecruitId = 1;
+const mineRecruitId = 0;
+const notMineRecruitId = 1;
 
 const meta: Meta<typeof RecruitDetailPage> = {
-  title: 'Page/Recruit/detail',
-  component: RecruitDetailPage as ComponentType<any>,
+  title: 'Page/Recruit/Detail',
+  component: RecruitDetailPage,
   decorators: [
     (Story) => (
       <PageLayout>
@@ -17,10 +34,102 @@ const meta: Meta<typeof RecruitDetailPage> = {
   parameters: {
     layout: 'fullscreen',
   },
+  argTypes: {
+    recruitId: {
+      options: [projectRecruitId, studyRecruitId],
+      control: {
+        type: 'radio',
+        labels: {
+          [0]: 'Project',
+          [1]: 'Study',
+        },
+      },
+    },
+  },
+  args: {
+    recruitId: projectRecruitId,
+  },
 };
 
 export default meta;
 
-type RecruitApplyPageStory = StoryObj<typeof RecruitDetailPage>;
+type RecruitDetailPageStory = StoryObj<typeof RecruitDetailPage>;
 
-export const Default: RecruitApplyPageStory = {};
+export const Success: RecruitDetailPageStory = {
+  decorators: [
+    (Story) => {
+      const setMyInfo = useSetMyInfo();
+      useEffect(() => {
+        setMyInfo(userInfo.certifiedSsafyUserInfo);
+      }, [setMyInfo]);
+
+      return <Story />;
+    },
+  ],
+};
+
+export const NotSignedIn: RecruitDetailPageStory = {
+  decorators: [
+    (Story) => {
+      const setMyInfo = useSetMyInfo();
+      useEffect(() => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        setMyInfo(null);
+      }, [setMyInfo]);
+
+      return <Story />;
+    },
+  ],
+};
+
+export const NotMine: RecruitDetailPageStory = {
+  args: { recruitId: mineRecruitId },
+  argTypes: { ...disableArgs(['recruitId']) },
+  render: function Render(args) {
+    const { recruitId } = args;
+    const setMyInfo = useSetMyInfo();
+    const setRecruitDetail = useSetRecruitDetail(recruitId);
+
+    useEffect(() => {
+      setMyInfo(userInfo.certifiedSsafyUserInfo);
+
+      setRecruitDetail((p) => {
+        if (!p) return;
+        return { ...p, mine: false };
+      });
+    }, [setRecruitDetail, setMyInfo]);
+
+    return <RecruitDetailPage {...args} />;
+  },
+};
+
+export const Mine: RecruitDetailPageStory = {
+  args: { recruitId: notMineRecruitId },
+  argTypes: { ...disableArgs(['recruitId']) },
+  render: function Render(args) {
+    const setMyInfo = useSetMyInfo();
+
+    useEffect(() => {
+      setMyInfo(userInfo.certifiedSsafyUserInfo);
+    }, [setMyInfo]);
+
+    return <RecruitDetailPage {...args} />;
+  },
+};
+
+export const Error: RecruitDetailPageStory = {
+  parameters: {
+    msw: {
+      handlers: {
+        recruit: [
+          getRecruitDetail,
+          getRecruitParticipantsError,
+          scrapRecruitError,
+          removeRecruitError,
+        ],
+        report: [reportError],
+      },
+    },
+  },
+};
