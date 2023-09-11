4f710fcd332d31419e0518f39c3e864911440342
Cola <bcad1591@gmail.com>
2023-09-11 21:19:17 +0900

    chore: 스킬 아이콘 assets

--- src/components/Common/SkillIcon/index.tsx
+++ src/components/Common/SkillIcon/index.tsx
@@ -1,17 +1,31 @@
-import type { SkillName } from '~/services/recruit';
+import { SkillName } from '~/services/recruit';
 
 import { Skills } from './skills';
 
 export interface SkillIconProps {
   name: SkillName;
   size?: number;
+  invert?: boolean;
 }
 
 const SkillIcon = (props: SkillIconProps) => {
-  const { name, size = 24 } = props;
+  const { name, size = 24, invert = false } = props;
   const SkillSVG = Skills[name];
+  const style = {
+    height: size,
+    filter: invert && invertable[name] ? 'invert(100%)' : undefined,
+  };
 
-  return <SkillSVG style={{ height: size }} />;
+  return <SkillSVG style={style} />;
+};
+
+const invertable = {
+  ...(Object.fromEntries(
+    Object.values(SkillName).map((skillName) => [skillName, false])
+  ) as Record<SkillName, boolean>),
+  [SkillName.IOS]: true,
+  [SkillName.NEXTJS]: true,
+  [SkillName.ETC]: true,
 };
 
 export default SkillIcon;
