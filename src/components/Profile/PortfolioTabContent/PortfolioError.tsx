af466a544a0804e1d3709be628c722fdb6bcdeba
Cola <bcad1591@gmail.com>
2023-09-17 22:50:39 +0900

    refactor: PortfolioError 업데이트
    - css 업데이트

--- src/components/Profile/PortfolioTabContent/PortfolioError.tsx
+++ src/components/Profile/PortfolioTabContent/PortfolioError.tsx
@@ -2,8 +2,9 @@ import { css } from '@emotion/react';
 import * as Sentry from '@sentry/nextjs';
 import { isAxiosError } from 'axios';
 
-import { SsafyIcon, TrackSize } from '~/components/Common';
-import { flex, fontCss, palettes } from '~/styles/utils';
+import { AlertText, TrackSize } from '~/components/Common';
+import { ErrorMessageWithSsafyIcon } from '~/components/ErrorMessageWithSsafyIcon';
+import { flex } from '~/styles/utils';
 import { getErrorResponse, isDevMode, ResponseCode } from '~/utils';
 
 interface PortfolioErrorProps {
@@ -21,8 +22,10 @@ const PortfolioError = (props: PortfolioErrorProps) => {
     if (code === ResponseCode.NO_PERMISSIONS) {
       return (
         <div css={selfCss}>
-          <SsafyIcon.Track size={TrackSize.LG2} />
-          <p css={errorMessageCss}>포트폴리오 조회 권한이 없습니다</p>
+          <ErrorMessageWithSsafyIcon
+            message="포트폴리오 조회 권한이 없습니다"
+            iconSize={TrackSize.LG2}
+          />
         </div>
       );
     }
@@ -34,18 +37,11 @@ const PortfolioError = (props: PortfolioErrorProps) => {
 
   return (
     <div css={selfCss}>
-      <p css={errorMessageCss}>포트폴리오 조회중 오류가 발생했습니다</p>
+      <AlertText size="md">포트폴리오 조회중 오류가 발생했습니다</AlertText>
     </div>
   );
 };
 
 const selfCss = css({ minHeight: 400 }, flex('center', 'center', 'column', 32));
-
-const errorMessageCss = css(
-  {
-    color: palettes.primary.default,
-  },
-  fontCss.style.B16
-);
 
 export default PortfolioError;
