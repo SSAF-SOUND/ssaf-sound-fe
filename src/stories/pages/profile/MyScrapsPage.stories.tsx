02bb8780cb472a55622b0507c016d6b9d8d74ce9
Cola <bcad1591@gmail.com>
2023-09-18 19:26:05 +0900

    feat: 나의 스크랩 빈 데이터 스토리 추가

--- src/stories/pages/profile/MyScrapsPage.stories.tsx
+++ src/stories/pages/profile/MyScrapsPage.stories.tsx
@@ -1,7 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import type { InfiniteData } from '@tanstack/query-core';
+import type { GetMyScrapedArticlesApiData } from '~/services/article';
+import type { GetMyScrapedRecruitsApiData } from '~/services/recruit';
+
+import { useQueryClient } from '@tanstack/react-query';
 
 import { userInfo } from '~/mocks/handlers/member/data';
 import MyScrapsPage from '~/pages/profile/my-scraps';
+import { queryKeys } from '~/react-query/common';
 import { useSetMyInfo } from '~/services/member';
 import { PageLayout } from '~/stories/Layout';
 
@@ -9,11 +15,15 @@ const meta: Meta<typeof MyScrapsPage> = {
   title: 'Page/Profile/MyScraps',
   component: MyScrapsPage,
   decorators: [
-    (Story) => (
-      <PageLayout>
-        <Story />
-      </PageLayout>
-    ),
+    (Story) => {
+      const setMyInfo = useSetMyInfo();
+      setMyInfo(mockUser);
+      return (
+        <PageLayout>
+          <Story />
+        </PageLayout>
+      );
+    },
   ],
   parameters: {
     layout: 'fullscreen',
@@ -24,11 +34,44 @@ export default meta;
 
 type MyScrapsPageStory = StoryObj<typeof MyScrapsPage>;
 
+const mockUser = userInfo.certifiedSsafyUserInfo;
+const myScrapedArticlesQueryKey = queryKeys.articles.myScraped();
+const myScrapedRecruitsQueryKey = queryKeys.recruit.myScraped();
+const emptyMyScrapedArticles: InfiniteData<
+  GetMyScrapedArticlesApiData['data']
+> = {
+  pages: [{ cursor: null, posts: [] }],
+  pageParams: [null],
+};
+const emptyMyScrapedRecruits: InfiniteData<
+  GetMyScrapedRecruitsApiData['data']
+> = {
+  pages: [{ nextCursor: null, recruits: [], isLast: true }],
+  pageParams: [null],
+};
 export const Default: MyScrapsPageStory = {
   decorators: [
     (Story) => {
-      const setMyInfo = useSetMyInfo();
-      setMyInfo(userInfo.certifiedSsafyUserInfo);
+      const queryClient = useQueryClient();
+      queryClient.removeQueries(myScrapedArticlesQueryKey);
+      queryClient.removeQueries(myScrapedRecruitsQueryKey);
+      return <Story />;
+    },
+  ],
+};
+
+export const Empty: MyScrapsPageStory = {
+  decorators: [
+    (Story) => {
+      const queryClient = useQueryClient();
+      queryClient.setQueryData(
+        myScrapedArticlesQueryKey,
+        emptyMyScrapedArticles
+      );
+      queryClient.setQueryData(
+        myScrapedRecruitsQueryKey,
+        emptyMyScrapedRecruits
+      );
       return <Story />;
     },
   ],
