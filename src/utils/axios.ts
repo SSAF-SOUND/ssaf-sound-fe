96d4b2e4722eba9e4484e763054d3f99d0b22c75
Cola <bcad1591@gmail.com>
2023-09-19 09:06:40 +0900

    refactor: configurePrivateAxiosInterceptors 파라미터 변경
    - 옵션객체에서 skipReissueToken을 전달할 수 있도록 변경
    - 옵션객체로 토큰 재발급을 트리거할 ResponseCode를 전달할 수 있도록 변경

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,4 +1,8 @@
-import type { InternalAxiosRequestConfig, AxiosError } from 'axios';
+import type {
+  InternalAxiosRequestConfig,
+  AxiosError,
+  AxiosInstance,
+} from 'axios';
 import type { ApiErrorResponse } from '~/types';
 
 import * as Sentry from '@sentry/nextjs';
@@ -46,28 +50,40 @@ const detectRequestInfiniteLoop = createRequestInfiniteLoopDetector(5, {
   },
 });
 
-const configurePublicAxiosInterceptors = () => {
+export const configurePublicAxiosInterceptors = (
+  axiosInstance: AxiosInstance
+) => {
   if (isDevMode) {
-    publicAxios.interceptors.request.use((config) => {
+    axiosInstance.interceptors.request.use((config) => {
       devPlugin(config);
       return config;
     });
   }
 };
 
-const configurePrivateAxiosInterceptors = (
+interface ConfigurePrivateAxiosInterceptorsOptions {
+  /** Token 재발급을 트리거할 ResponseCode  */
+  reissueTokenTriggerCodes: string[];
+  /** Token 재발급이 트리거되었을 때, 재발급을 생략할지 여부를 `boolean`으로 반환하는 함수 */
+  skipReissueToken?: () => boolean;
+}
+
+export const configurePrivateAxiosInterceptors = (
+  axiosInstance: AxiosInstance,
   reissueToken: () => Promise<unknown>,
-  responseCodesTriggerReissueToken: string[]
+  options: ConfigurePrivateAxiosInterceptorsOptions
 ) => {
   let reissueTokenRequest: ReturnType<typeof reissueToken> | undefined;
   const waitingQueue = new Set<InternalAxiosRequestConfig>();
+  const { reissueTokenTriggerCodes, skipReissueToken = () => false } = options;
+  const reissueTokenTriggerCodesSet = new Set<string>(reissueTokenTriggerCodes);
 
   /**
-   * - 토큰 재발급 수행 중에 privateAxios 요청이 감지된 경우, 해당 요청을 서버에 보내지 않고 대기시킵니다.
+   * - 토큰 재발급 수행 중에 axiosInstance 요청이 감지된 경우, 해당 요청을 서버에 보내지 않고 대기시킵니다.
    * - 토큰 재발급에 성공하면, 대기중인 요청을 서버에 보냅니다.
    * - 토큰 재발급에 실패하면, 대기중인 요청을 서버에 보내지 않고 즉시 실패시킵니다.
    */
-  privateAxios.interceptors.request.use((config) => {
+  axiosInstance.interceptors.request.use((config) => {
     devPlugin(config);
 
     if (reissueTokenRequest === undefined) return config;
@@ -75,12 +91,12 @@ const configurePrivateAxiosInterceptors = (
   });
 
   /**
-   * - privateAxios 요청에 대한 응답 데이터에 토큰 재발급이 필요하다는 code가 포함되었다면,
+   * - axiosInstance 요청에 대한 응답 데이터에 토큰 재발급이 필요하다는 code가 포함되었다면,
    * 토큰 재발급을 수행하고, Reject될 예정이었던 요청을 대기시킵니다.
    * - 토큰 재발급에 성공한 경우, 대기중인 요청을 다시 서버로 전송합니다.
    * - 토큰 재발급에 실패한 경우, 대기중인 요청을 실패시킵니다.
    */
-  privateAxios.interceptors.response.use(
+  axiosInstance.interceptors.response.use(
     undefined,
     (error: AxiosError<ApiErrorResponse> | Error) => {
       const tag = `[In privateAxios response interceptor]`;
@@ -90,22 +106,24 @@ const configurePrivateAxiosInterceptors = (
       }
 
       const requestConfig = error.config;
-      const retryFailedRequest = () => privateAxios.request(requestConfig);
-
+      const retryFailedRequest = () => axiosInstance.request(requestConfig);
       const statusCode = error.response.status;
+
       if (statusCode >= 500) {
         console.error(`${tag}: Server Error`);
         return Promise.reject(error);
       }
 
-      console.error(`${tag}: Client Error`);
       const response = error.response.data;
+      const errorMessage = response?.message;
 
-      if (!responseCodesTriggerReissueToken.includes(response.code)) {
+      if (!reissueTokenTriggerCodesSet.has(response.code)) {
+        console.error(`${tag}: ${errorMessage}`);
         return Promise.reject(error);
       }
 
-      if (detectRequestInfiniteLoop()) return;
+      console.error(`${tag}: Token Expired`);
+      if (skipReissueToken()) return;
 
       if (reissueTokenRequest === undefined) {
         reissueTokenRequest = reissueToken();
@@ -125,6 +143,9 @@ const configurePrivateAxiosInterceptors = (
   );
 };
 
-configurePrivateAxiosInterceptors(reissueToken, [ResponseCode.EXPIRED_TOKEN]);
+configurePrivateAxiosInterceptors(privateAxios, reissueToken, {
+  reissueTokenTriggerCodes: [ResponseCode.EXPIRED_TOKEN],
+  skipReissueToken: detectRequestInfiniteLoop,
+});
 
-configurePublicAxiosInterceptors();
+configurePublicAxiosInterceptors(publicAxios);
