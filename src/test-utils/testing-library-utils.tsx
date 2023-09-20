b9f52f5fb183b04a7cb43f8ac84b5a6a49ae9bcb
Cola <bcad1591@gmail.com>
2023-09-20 21:59:50 +0900

    refactor: testing library utils

--- src/test-utils/testing-library-utils.tsx
+++ src/test-utils/testing-library-utils.tsx
@@ -1,24 +1,40 @@
+import type { DehydratedState, QueryClient } from '@tanstack/react-query';
 import type { RenderOptions } from '@testing-library/react';
+import type { NextPageAuthConfig } from 'next/types';
 import type { ReactElement, ReactNode } from 'react';
 
-import { QueryClientProvider } from '@tanstack/react-query';
+import { Hydrate, QueryClientProvider } from '@tanstack/react-query';
 import { render } from '@testing-library/react';
+import { Toaster } from 'react-hot-toast';
 
+import AuthChecker from '~/components/AuthChecker';
+import { GlobalModal } from '~/components/GlobalModal';
 import { getQueryClient } from '~/react-query/common';
 
-const AllProviders = ({ children }: { children: ReactNode }) => {
-  return (
-    <QueryClientProvider client={getQueryClient()}>
-      {children}
-    </QueryClientProvider>
-  );
-};
+export interface RenderCustomOptions {
+  queryClient?: QueryClient;
+  dehydratedState?: DehydratedState;
+  auth?: NextPageAuthConfig;
+}
 
 const renderWithContext = (
   ui: ReactElement,
-  options?: Omit<RenderOptions, 'wrapper'>
+  options: RenderOptions = {},
+  customOptions: RenderCustomOptions = {}
 ) => {
-  return render(ui, { wrapper: AllProviders, ...options });
+  const { queryClient, dehydratedState, auth } = customOptions;
+  const ourQueryClient = queryClient ?? getQueryClient();
+
+  return render(
+    <QueryClientProvider client={ourQueryClient}>
+      <Hydrate state={dehydratedState}>
+        <Toaster />
+        {auth ? <AuthChecker auth={auth}>{ui}</AuthChecker> : <>{ui}</>}
+        <GlobalModal />
+      </Hydrate>
+    </QueryClientProvider>,
+    options
+  );
 };
 
 export * from '@testing-library/react';
