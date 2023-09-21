060747214fba56492709c69d97daf6c7d560c086
Cola <bcad1591@gmail.com>
2023-09-22 07:25:10 +0900

    test: 로그인 페이지

--- src/stories/pages/auth/SignInPage.stories.tsx
+++ src/stories/pages/auth/SignInPage.stories.tsx
@@ -1,16 +1,25 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { getMyInfo } from '~/mocks/handlers';
+import { expect } from '@storybook/jest';
+import { waitForElementToBeRemoved, within } from '@storybook/testing-library';
+
+import { loaderText } from '~/components/Common';
 import {
   mockGetCertifiedSsafyMyInfo,
   mockGetMyInfoError,
 } from '~/mocks/handlers/member/apis/mockGetMyInfo';
 import SignInPage from '~/pages/auth/sign-in';
 import { PageLayout } from '~/stories/Layout';
+import {
+  createMockRouterMethods,
+  getMockRouter,
+} from '~/stories/utils/mockRouter';
+import { useAutoSignOut } from '~/stories/utils/useAutoSignOut';
 import { useResetQueriesEffect } from '~/stories/utils/useResetQueriesEffect';
+import { routes, sleep, toMs } from '~/utils';
 
 const meta: Meta<typeof SignInPage> = {
-  title: 'Page/Sign In',
+  title: 'Page/Auth/Sign In',
   component: SignInPage,
   decorators: [
     (Story) => {
@@ -37,6 +46,36 @@ export const NotSignedIn: SignInPageStory = {
       },
     },
   },
+  decorators: [
+    (Story) => {
+      useAutoSignOut();
+      return <Story />;
+    },
+  ],
+  play: async ({ canvasElement, step }) => {
+    const canvas = within(canvasElement);
+
+    await waitForElementToBeRemoved(canvas.getByText(loaderText.checkUser), {
+      timeout: 5000,
+    });
+
+    await step(
+      'Google, GitHub, Kakao, Apple 로그인 버튼이 보인다.',
+      async () => {
+        const allSignInLinks = await canvas.findAllByText(
+          /(google|github|kakao|apple) 로그인/i
+        );
+        await expect(allSignInLinks).toHaveLength(4);
+      }
+    );
+
+    await step('메인 페이지 링크가 보인다.', () => {
+      const toMainPageLink = canvas.getByRole('link', {
+        name: /메인페이지/,
+      });
+      expect(toMainPageLink).toHaveAttribute('href', routes.main());
+    });
+  },
 };
 
 export const SignedIn: SignInPageStory = {
@@ -46,5 +85,16 @@ export const SignedIn: SignInPageStory = {
         member: [mockGetCertifiedSsafyMyInfo],
       },
     },
+    nextjs: {
+      router: createMockRouterMethods(),
+    },
+  },
+  play: async ({ parameters, step }) => {
+    const router = getMockRouter(parameters);
+    await sleep(toMs(1));
+
+    step('로그인 상태라면 메인 페이지로 리다이렉션된다', () => {
+      expect(router.replace).toBeCalledWith(routes.main());
+    });
   },
 };
