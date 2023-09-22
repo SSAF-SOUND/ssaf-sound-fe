a67bbffe104b21dcc1ec4c01a6ab107843f01646
Cola <bcad1591@gmail.com>
2023-09-23 04:35:18 +0900

    chore: 주석 추가

--- src/test-utils/resizeObserver.mock.ts
+++ src/test-utils/resizeObserver.mock.ts
@@ -1,3 +1,5 @@
+/** https://github.com/ZeeCoder/use-resize-observer/issues/40#issuecomment-1540994404 */
+
 import ResizeObserver from 'resize-observer-polyfill';
 
 export const createMockResizeObserver = () => {
