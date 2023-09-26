1632a7f8c201d45dbdd4c399ff35e67d11052339
Cola <bcad1591@gmail.com>
2023-09-27 06:02:15 +0900

    feat: withRetryButton prop 추가, css 업데이트

--- src/components/PreviewErrorCard/index.tsx
+++ src/components/PreviewErrorCard/index.tsx
@@ -1,27 +1,41 @@
 import { css } from '@emotion/react';
 
-import { Button } from '~/components/Common';
+import { AlertText, Button } from '~/components/Common';
 import { flex, palettes } from '~/styles/utils';
 
 interface PreviewErrorCardProps {
   className?: string;
-  onClickRetry: () => void;
+  withRetryButton?: boolean;
+  onClickRetry?: () => void;
+  errorMessage?: string;
 }
 
 export const PreviewErrorCard = (props: PreviewErrorCardProps) => {
-  const { className, onClickRetry } = props;
+  const {
+    className,
+    onClickRetry,
+    withRetryButton = false,
+    errorMessage = '오류가 발생했습니다.',
+  } = props;
   return (
     <div css={selfCss} className={className}>
-      <p css={{ marginBottom: 12 }}>오류가 발생했습니다.</p>
-      <Button
-        theme="error"
-        css={{ width: 130, color: palettes.white }}
-        onClick={onClickRetry}
-      >
-        재시도
-      </Button>
+      <AlertText css={{ marginBottom: 12 }} size="sm">
+        {errorMessage}
+      </AlertText>
+      {withRetryButton && (
+        <Button
+          theme="error"
+          css={{ width: 130, color: palettes.white }}
+          onClick={onClickRetry}
+        >
+          재시도
+        </Button>
+      )}
     </div>
   );
 };
 
-const selfCss = css({ width: '100%', height: 140 }, flex('center', 'center'));
+const selfCss = css(
+  { width: '100%', height: 140, textAlign: 'center', whiteSpace: 'pre-wrap' },
+  flex('center', 'center')
+);
