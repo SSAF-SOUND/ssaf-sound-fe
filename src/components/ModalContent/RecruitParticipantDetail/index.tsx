15aff4cb4ac30a5d8ebeef9ed5a6a319a72dced5
Cola <bcad1591@gmail.com>
2023-09-28 03:59:38 +0900

    feat: 리쿠르팅 참여자 디테일 모달 업데이트
    - 리쿠르팅 모집자가 대상인 경우 header 영역 메세지 다르게 보여주기 + 지원서 보러가기 및 멤버제외 버튼 가리기
    - 멤버 제외시 모달 팝업

--- src/components/ModalContent/RecruitParticipantDetail/index.tsx
+++ src/components/ModalContent/RecruitParticipantDetail/index.tsx
@@ -1,9 +1,11 @@
+import type { UserInfo } from '~/services/member';
 import type { RecruitParticipantUserInfo } from '~/services/recruit';
 
 import { css } from '@emotion/react';
 
 import { Button, Icon, IconButton, Modal } from '~/components/Common';
 import { FullDateTime } from '~/components/FullDateTime';
+import { Alert } from '~/components/ModalContent';
 import SquareAvatar from '~/components/SquareAvatar';
 import { useAsyncState } from '~/hooks';
 import { flex, fontCss, palettes, position, Theme } from '~/styles/utils';
@@ -11,6 +13,7 @@ import { noop } from '~/utils';
 
 interface RecruitParticipantDetailProps {
   userInfo: RecruitParticipantUserInfo;
+  isRecruitAuthor?: boolean;
   showPrivateButtons?: boolean;
   onClickClose?: () => void;
   onClickUserProfileLink?: () => void;
@@ -23,6 +26,7 @@ export const RecruitParticipantDetail = (
 ) => {
   const {
     userInfo,
+    isRecruitAuthor = false,
     showPrivateButtons = false,
     onClickClose,
     onClickUserProfileLink,
@@ -47,12 +51,16 @@ export const RecruitParticipantDetail = (
       </div>
 
       <div>
-        <div>
-          <FullDateTime
-            dateTimeString={joinedAt}
-            suffix={'에 멤버가 되었습니다.'}
-          />
-        </div>
+        <header css={[headerCss, { marginBottom: 4 }]}>
+          {isRecruitAuthor ? (
+            <p>리쿠르팅 모집자입니다.</p>
+          ) : (
+            <FullDateTime
+              dateTimeString={joinedAt}
+              suffix={'에 멤버가 되었습니다.'}
+            />
+          )}
+        </header>
 
         <div css={{ marginBottom: 16 }}>
           <SquareAvatar style={{ width: '100%' }} userInfo={userInfo} />
@@ -68,26 +76,26 @@ export const RecruitParticipantDetail = (
             프로필 보러가기
           </Button>
 
-          {showPrivateButtons && (
+          {!isRecruitAuthor && showPrivateButtons && (
             <>
-              <Button
-                css={[{ backgroundColor: palettes.primary.light }, buttonCss]}
-                size="md"
-                theme={Theme.PRIMARY}
-                onClick={onClickRecruitApplicationLink}
-              >
-                지원서 보러가기
-              </Button>
+              {
+                <Button
+                  css={[{ backgroundColor: palettes.primary.light }, buttonCss]}
+                  size="md"
+                  theme={Theme.PRIMARY}
+                  onClick={onClickRecruitApplicationLink}
+                >
+                  지원서 보러가기
+                </Button>
+              }
 
-              <Button
-                size="md"
-                theme={Theme.PRIMARY}
-                css={buttonCss}
-                loading={isExcludingRecruitParticipant}
-                onClick={handleExcludeRecruitParticipant}
-              >
-                멤버 제외하기
-              </Button>
+              <RecruitParticipantExcludeButton
+                userInfo={userInfo}
+                isButtonLoading={isExcludingRecruitParticipant}
+                handleExcludeRecruitParticipant={
+                  handleExcludeRecruitParticipant
+                }
+              />
             </>
           )}
         </div>
@@ -108,3 +116,42 @@ const selfCss = css(
 
 const buttonGroupCss = css(flex('', '', 'column', 6));
 const buttonCss = css(fontCss.style.B12);
+const headerCss = css(fontCss.style.R12, { color: palettes.font.blueGrey });
+
+interface RecruitParticipantExcludeButtonProps {
+  userInfo: UserInfo;
+  isButtonLoading?: boolean;
+  handleExcludeRecruitParticipant?: () => void;
+}
+
+const RecruitParticipantExcludeButton = (
+  props: RecruitParticipantExcludeButtonProps
+) => {
+  const { userInfo, isButtonLoading, handleExcludeRecruitParticipant } = props;
+
+  const alertDescription = `${userInfo.nickname}님을 리쿠르팅 멤버에서 제외합니다.`;
+
+  return (
+    <Modal
+      trigger={
+        <Button
+          size="md"
+          theme={Theme.PRIMARY}
+          css={buttonCss}
+          loading={isButtonLoading}
+        >
+          멤버 제외하기
+        </Button>
+      }
+      content={
+        <Alert
+          actionText="제외"
+          cancelText="취소"
+          title="알림"
+          description={alertDescription}
+          onClickAction={handleExcludeRecruitParticipant}
+        />
+      }
+    />
+  );
+};
