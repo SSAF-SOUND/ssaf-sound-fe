381ebdf8363c74c281f76ddb144f0bb2323d485f
Cola <bcad1591@gmail.com>
2023-09-28 05:13:31 +0900

    chore: top level describe 제거

--- cypress/integrations/pages/certification/student-certification.cy.ts
+++ cypress/integrations/pages/certification/student-certification.cy.ts
@@ -6,31 +6,29 @@ const skipToSubmitAnswer = (value: string) => {
   cy.findByRole('button', { name: '확인' }).realClick();
 };
 
-describe('student-certification page', () => {
-  it('정답을 입력하는 경우 인증 완료 알림창이 팝업되고, 확인 버튼을 누르면 생성된 유저의 네임카드를 볼 수 있다.', () => {
-    cy.visitStory('page-certification-student-certification--correct-answer');
+it('정답을 입력하는 경우 인증 완료 알림창이 팝업되고, 확인 버튼을 누르면 생성된 유저의 네임카드를 볼 수 있다.', () => {
+  cy.visitStory('page-certification-student-certification--correct-answer');
 
-    skipToSubmitAnswer(answerValue);
-    cy.findByRole('dialog').should('be.exist');
-    cy.findByRole('dialog').findByRole('button', { name: '확인' }).realClick();
-    cy.findByText(/프로필이 생성되었습니다/).should('be.exist');
+  skipToSubmitAnswer(answerValue);
+  cy.findByRole('dialog').should('be.exist');
+  cy.findByRole('dialog').findByRole('button', { name: '확인' }).realClick();
+  cy.findByText(/프로필이 생성되었습니다/).should('be.exist');
 
-    cy.get('a').should('have.attr', 'href', '/main');
-  });
+  cy.get('a').should('have.attr', 'href', '/main');
+});
 
-  it('오답을 입력하는 경우 오답 알림창이 팝업된다.', () => {
-    cy.visitStory('page-certification-student-certification--incorrect-answer');
+it('오답을 입력하는 경우 오답 알림창이 팝업된다.', () => {
+  cy.visitStory('page-certification-student-certification--incorrect-answer');
 
-    skipToSubmitAnswer(answerValue);
-    cy.findByRole('dialog').should('be.exist');
-    cy.findByRole('dialog').findByRole('button', { name: '확인' }).realClick();
-    cy.findByRole('textbox').should('be.exist');
-  });
+  skipToSubmitAnswer(answerValue);
+  cy.findByRole('dialog').should('be.exist');
+  cy.findByRole('dialog').findByRole('button', { name: '확인' }).realClick();
+  cy.findByRole('textbox').should('be.exist');
+});
 
-  it('시도 횟수를 초과했는데 입력하는 경우 알림 메세지가 팝업된다.', () => {
-    cy.visitStory('page-certification-student-certification--no-more-attempts');
+it('시도 횟수를 초과했는데 입력하는 경우 알림 메세지가 팝업된다.', () => {
+  cy.visitStory('page-certification-student-certification--no-more-attempts');
 
-    skipToSubmitAnswer(answerValue);
-    cy.findByRole('status').should('be.exist');
-  });
+  skipToSubmitAnswer(answerValue);
+  cy.findByRole('status').should('be.exist');
 });
