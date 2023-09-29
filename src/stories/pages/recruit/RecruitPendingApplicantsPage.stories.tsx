baa78955bf088f2488264af887af84044353db35
Cola <bcad1591@gmail.com>
2023-09-30 03:12:46 +0900

    refactor: story 재작성

--- src/stories/pages/recruit/RecruitPendingApplicantsPage.stories.tsx
+++ src/stories/pages/recruit/RecruitPendingApplicantsPage.stories.tsx
@@ -1,89 +1,132 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import type { RecruitDetail } from '~/services/recruit';
 
-import { useQueryClient } from '@tanstack/react-query';
-
-import { userInfo } from '~/mocks/handlers/member/data';
+import { mockGetCertifiedSsafyMyInfo } from '~/mocks/handlers/member/apis/mockGetMyInfo';
+import { mockUserInfo } from '~/mocks/handlers/member/data';
+import { createMockGetRecruitApplicants } from '~/mocks/handlers/recruit/apis/mockGetRecruitApplicants';
+import { createMockGetRecruitDetail } from '~/mocks/handlers/recruit/apis/mockGetRecruitDetail';
+import { createMockGetRecruitParticipants } from '~/mocks/handlers/recruit/apis/mockGetRecruitParticipants';
+import { mockLikeRecruitApplication } from '~/mocks/handlers/recruit/apis/mockLikeRecruitApplication';
+import { mockRejectRecruitApplication } from '~/mocks/handlers/recruit/apis/mockRejectRecruitApplication';
 import {
   createMockRecruitApplicants,
   createMockRecruitDetail,
   createMockRecruitParticipants,
 } from '~/mocks/handlers/recruit/data';
 import RecruitPendingApplicantsPage from '~/pages/recruits/[recruitId]/applications';
-import { queryKeys } from '~/react-query/common';
-import { useSetMyInfo } from '~/services/member';
 import { PageLayout } from '~/stories/Layout';
+import { createMswParameters } from '~/stories/utils';
+
+const recruitId = 10000;
+const myInfo = mockUserInfo.certifiedSsafyUserInfo;
+const otherUserInfo = mockUserInfo.uncertifiedSsafyUserInfo;
+const mockProjectDetail = createMockRecruitDetail(recruitId, false, {
+  mine: true,
+  author: myInfo,
+  completed: false,
+});
+const mockStudyDetail = createMockRecruitDetail(recruitId, true, {
+  mine: true,
+  author: myInfo,
+  completed: false,
+});
 
 const meta: Meta<typeof RecruitPendingApplicantsPage> = {
-  title: 'Page/Recruit/Applicants/Pending',
+  title: 'Page/Recruit/리쿠르팅 신청서 목록',
   component: RecruitPendingApplicantsPage,
   decorators: [
-    (Story) => {
-      return (
-        <PageLayout>
-          <Story />
-        </PageLayout>
-      );
-    },
+    (Story) => (
+      <PageLayout>
+        <Story />
+      </PageLayout>
+    ),
   ],
   parameters: {
     layout: 'fullscreen',
+    ...createMswParameters({
+      member: [mockGetCertifiedSsafyMyInfo],
+      recruit: [
+        createMockGetRecruitApplicants(
+          createMockRecruitApplicants(mockProjectDetail)
+        ),
+        createMockGetRecruitParticipants(
+          createMockRecruitParticipants(mockProjectDetail)
+        ),
+        createMockGetRecruitDetail(mockProjectDetail),
+      ],
+      common: [mockLikeRecruitApplication, mockRejectRecruitApplication],
+    }),
+  },
+  args: {
+    recruitId,
   },
 };
-
-const recruitId = NaN;
-const recruitDetailQueryKey = queryKeys.recruit.detail(recruitId);
-const recruitApplicantsQueryKey =
-  queryKeys.recruit.application.applicants(recruitId);
-const recruitParticipantsQueryKey = queryKeys.recruit.participants(recruitId);
-
-const mockProjectDetail = createMockRecruitDetail(recruitId, false, {
-  mine: true,
-});
-const mockProjectApplicants = createMockRecruitApplicants(recruitId, {
-  recruitDetail: mockProjectDetail,
-});
-const mockProjectParticipants =
-  createMockRecruitParticipants(mockProjectDetail);
-
-//
-const mockStudyDetail = createMockRecruitDetail(recruitId, true, {
-  mine: true,
-});
-const mockStudyApplicants = createMockRecruitApplicants(recruitId, {
-  recruitDetail: mockStudyDetail,
-});
-const mockStudyParticipants = createMockRecruitParticipants(mockStudyDetail);
 
 export default meta;
 
-type RecruitApplicantsStoryArgs = {
-  isStudy: boolean;
+type RecruitPendingApplicantsPageStory = StoryObj<
+  typeof RecruitPendingApplicantsPage
+>;
+
+export const PendingRecruit: RecruitPendingApplicantsPageStory = {
+  name: '모집중인 리쿠르팅의 신청서 목록',
 };
 
-type RecruitPendingApplicantsPageStory = StoryObj<RecruitApplicantsStoryArgs>;
+const completedProjectDetail: RecruitDetail = {
+  ...mockProjectDetail,
+  finishedRecruit: true,
+};
 
-export const Default: RecruitPendingApplicantsPageStory = {
-  args: {
-    isStudy: false,
+export const CompletedRecruit: RecruitPendingApplicantsPageStory = {
+  name: '종료된 리쿠르팅의 신청서 목록',
+  parameters: {
+    ...createMswParameters({
+      recruit: [
+        createMockGetRecruitApplicants(
+          createMockRecruitApplicants(completedProjectDetail)
+        ),
+        createMockGetRecruitParticipants(
+          createMockRecruitParticipants(completedProjectDetail)
+        ),
+        createMockGetRecruitDetail(completedProjectDetail),
+      ],
+    }),
   },
-  render: function Render(args) {
-    const { isStudy = false } = args;
-    const queryClient = useQueryClient();
-    const setMyInfo = useSetMyInfo();
-    setMyInfo(userInfo.certifiedSsafyUserInfo);
-    queryClient.setQueryData(
-      recruitDetailQueryKey,
-      isStudy ? mockStudyDetail : mockProjectDetail
-    );
-    queryClient.setQueryData(
-      recruitApplicantsQueryKey,
-      isStudy ? mockStudyApplicants : mockProjectApplicants
-    );
-    queryClient.setQueryData(
-      recruitParticipantsQueryKey,
-      isStudy ? mockStudyParticipants : mockProjectParticipants
-    );
+};
 
-    return <RecruitPendingApplicantsPage />;
+export const Study: RecruitPendingApplicantsPageStory = {
+  name: '스터디 리쿠르팅의 신청서 목록',
+  parameters: {
+    ...createMswParameters({
+      recruit: [
+        createMockGetRecruitApplicants(
+          createMockRecruitApplicants(mockStudyDetail)
+        ),
+        createMockGetRecruitParticipants(
+          createMockRecruitParticipants(mockStudyDetail)
+        ),
+        createMockGetRecruitDetail(mockStudyDetail),
+      ],
+    }),
+  },
+};
+
+const notMyRecruitDetail: RecruitDetail = {
+  ...mockProjectDetail,
+  mine: false,
+  author: otherUserInfo,
+};
+
+export const NotMine: RecruitPendingApplicantsPageStory = {
+  name: '내 리쿠르팅이 아님',
+  parameters: {
+    ...createMswParameters({
+      recruit: [
+        createMockGetRecruitApplicants(
+          createMockRecruitApplicants(notMyRecruitDetail)
+        ),
+        createMockGetRecruitDetail(notMyRecruitDetail),
+      ],
+    }),
   },
 };
