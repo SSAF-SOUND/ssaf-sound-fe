0287fa67db1321d64c75fe7d4e027433dfa6e21d
Cola <bcad1591@gmail.com>
2023-09-29 21:32:32 +0900

    refactor: 테스트에 사용할 props 추가

--- src/pages/recruits/[recruitId]/applications/mine/index.tsx
+++ src/pages/recruits/[recruitId]/applications/mine/index.tsx
@@ -34,14 +34,19 @@ import {
 } from '~/utils';
 
 // MatchStatus
-// INITIAL -> 신청 페이지로 리다이렉션
 // 그 외 -> 페이지 조회 가능
 
+interface MyRecruitApplicationPageProps {
+  recruitId?: number;
+}
+
 const metaTitle = '내 리쿠르팅 신청서';
-const MyRecruitApplicationPage: CustomNextPage = () => {
+const MyRecruitApplicationPage: CustomNextPage<
+  MyRecruitApplicationPageProps
+> = (props) => {
   const router = useRouter();
   const query = router.query as Params;
-  const recruitId = Number(query.recruitId);
+  const recruitId = props.recruitId ?? Number(query.recruitId);
 
   const {
     data: recruitDetail,
