d3757e8a508e1cd994e8a7c216bc52ae4f512b29
Cola <bcad1591@gmail.com>
2023-09-29 22:58:59 +0900

    test: recruit application

---
+++ cypress/integrations/pages/recruits/recruit-application.cy.ts
@@ -0,0 +1,51 @@
+const assertExistsApproveButton = () => {
+  cy.findByRole('button', { name: /수락/ })
+    .as('approveButton')
+    .should('be.exist');
+};
+
+const assertExistsRejectButton = () => {
+  cy.findByRole('button', { name: /거절/ })
+    .as('rejectButton')
+    .should('be.exist');
+};
+
+const assertExistsApproveConfirmText = () => {
+  cy.contains('수락함').should('be.exist');
+};
+
+const assertExistsRejectConfirmText = () => {
+  cy.contains('거절함').should('be.exist');
+};
+
+it('내 리쿠르팅이 아니면, 해당 리쿠르팅에 대한 다른 사람의 신청서는 조회할 수 없다.', () => {
+  cy.visitStory('page-recruit-리쿠르팅-신청서--not-mine');
+
+  cy.contains('권한이 없어요').should('be.exist');
+});
+
+describe('응답 대기중인 신청서', () => {
+  beforeEach(() => {
+    cy.visitStory('page-recruit-리쿠르팅-신청서--pending-recruit-application');
+  });
+
+  it('수락할 수 있다.', () => {
+    assertExistsApproveButton();
+    cy.get('@approveButton').click();
+    assertExistsApproveConfirmText();
+  });
+
+  it('거절할 수 있다.', () => {
+    assertExistsRejectButton();
+    cy.get('@rejectButton').click();
+    assertExistsRejectConfirmText();
+  });
+});
+
+describe('모집이 종료될 때 까지 응답하지 않은 신청서', () => {
+  it('응답 안함 텍스트가 보인다.', () => {
+    cy.visitStory('page-recruit-리쿠르팅-신청서--initial-recruit-application');
+
+    cy.contains('응답 안함').should('be.exist');
+  });
+});
--- src/stories/pages/recruit/RecruitApplicationPage.stories.tsx
+++ src/stories/pages/recruit/RecruitApplicationPage.stories.tsx
@@ -1,80 +1,169 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import type { RecruitDetail } from '~/services/recruit';
 
-import { useQueryClient } from '@tanstack/react-query';
-
-import { userInfo } from '~/mocks/handlers/member/data';
+import { createMockGetMyInfo } from '~/mocks/handlers/member/apis/mockGetMyInfo';
+import { mockUserInfo } from '~/mocks/handlers/member/data';
+import { mockApproveRecruitApplication } from '~/mocks/handlers/recruit/apis/mockApproveRecruitApplication';
+import { createMockGetRecruitApplication } from '~/mocks/handlers/recruit/apis/mockGetRecruitApplication';
+import { createMockGetRecruitDetail } from '~/mocks/handlers/recruit/apis/mockGetRecruitDetail';
+import { mockRejectRecruitApplication } from '~/mocks/handlers/recruit/apis/mockRejectRecruitApplication';
 import {
   createMockRecruitApplication,
   createMockRecruitDetail,
 } from '~/mocks/handlers/recruit/data';
 import RecruitApplicationPage from '~/pages/recruits/[recruitId]/applications/[recruitApplicationId]';
-import { queryKeys } from '~/react-query/common';
-import { useSetMyInfo } from '~/services/member';
 import { MatchStatus, RecruitCategoryName } from '~/services/recruit';
 import { PageLayout } from '~/stories/Layout';
+import { createMswParameters } from '~/stories/utils';
+
+const myInfo = mockUserInfo.certifiedSsafyUserInfo;
+const otherUser = mockUserInfo.uncertifiedSsafyUserInfo;
+const recruitId = 10000;
+const recruitApplicationId = 10000;
+
+const mockProjectApplication = createMockRecruitApplication(recruitId, {
+  category: RecruitCategoryName.PROJECT,
+  recruitApplicationId,
+  author: otherUser,
+});
+
+const mockStudyApplication = createMockRecruitApplication(recruitId, {
+  category: RecruitCategoryName.STUDY,
+  recruitApplicationId,
+  author: otherUser,
+});
+
+const mockProjectDetail = createMockRecruitDetail(recruitId, false, {
+  mine: true,
+  author: myInfo,
+  completed: false,
+});
+
+const mockCompletedProjectDetail: RecruitDetail = {
+  ...mockProjectDetail,
+  finishedRecruit: true,
+};
+
+const mockStudyDetail = createMockRecruitDetail(recruitId, true, {
+  mine: true,
+  author: myInfo,
+  completed: false,
+});
 
 const meta: Meta<typeof RecruitApplicationPage> = {
-  title: 'Page/Recruit/Application',
+  title: 'Page/Recruit/리쿠르팅 신청서',
   component: RecruitApplicationPage,
   decorators: [
-    (Story) => {
-      return (
-        <PageLayout>
-          <Story />
-        </PageLayout>
-      );
-    },
+    (Story) => (
+      <PageLayout>
+        <Story />
+      </PageLayout>
+    ),
   ],
   parameters: {
     layout: 'fullscreen',
+    ...createMswParameters({
+      member: [createMockGetMyInfo(myInfo)],
+      common: [mockApproveRecruitApplication, mockRejectRecruitApplication],
+    }),
+  },
+  args: {
+    recruitId,
+    recruitApplicationId,
   },
 };
-
-const recruitId = NaN;
-const recruitDetailQueryKey = queryKeys.recruit.detail(recruitId);
-const mockProjectDetail = createMockRecruitDetail(recruitId, false, {
-  mine: true,
-  matchStatus: MatchStatus.INITIAL,
-});
-const mockStudyDetail = createMockRecruitDetail(recruitId, true, {
-  matchStatus: MatchStatus.INITIAL,
-  mine: true,
-});
-
-const myApplicationQueryKey = queryKeys.recruit.application.mine(recruitId);
-const mockProjectApplication = createMockRecruitApplication(recruitId, {
-  category: RecruitCategoryName.PROJECT,
-});
-const mockStudyApplication = createMockRecruitApplication(recruitId, {
-  category: RecruitCategoryName.STUDY,
-});
 
 export default meta;
 
-type RecruitApplicantsStoryArgs = {
-  isStudy: boolean;
+type RecruitApplicationPageStory = StoryObj<typeof RecruitApplicationPage>;
+
+export const NotMine: RecruitApplicationPageStory = {
+  name: '내 리쿠르팅이 아닌 경우',
+  parameters: {
+    ...createMswParameters({
+      recruit: [
+        createMockGetRecruitDetail({
+          ...mockProjectDetail,
+          mine: false,
+          author: otherUser,
+        }),
+        createMockGetRecruitApplication(mockProjectApplication),
+      ],
+    }),
+  },
 };
 
-type RecruitApplicationPageStory = StoryObj<RecruitApplicantsStoryArgs>;
-
-export const Default: RecruitApplicationPageStory = {
-  args: {
-    isStudy: false,
+export const PendingRecruitApplication: RecruitApplicationPageStory = {
+  name: '응답 대기',
+  parameters: {
+    ...createMswParameters({
+      recruit: [
+        createMockGetRecruitDetail(mockProjectDetail),
+        createMockGetRecruitApplication({
+          ...mockProjectApplication,
+          matchStatus: MatchStatus.PENDING,
+        }),
+      ],
+    }),
   },
-  render: function Render(args) {
-    const { isStudy = false } = args;
-    const queryClient = useQueryClient();
-    const setMyInfo = useSetMyInfo();
-    setMyInfo(userInfo.certifiedSsafyUserInfo);
-    queryClient.setQueryData(
-      recruitDetailQueryKey,
-      isStudy ? mockStudyDetail : mockProjectDetail
-    );
-    queryClient.setQueryData(
-      myApplicationQueryKey,
-      isStudy ? mockStudyApplication : mockProjectApplication
-    );
+};
 
-    return <RecruitApplicationPage />;
+export const RejectedRecruitApplication: RecruitApplicationPageStory = {
+  name: '거절함',
+  parameters: {
+    ...createMswParameters({
+      recruit: [
+        createMockGetRecruitDetail(mockProjectDetail),
+        createMockGetRecruitApplication({
+          ...mockProjectApplication,
+          matchStatus: MatchStatus.REJECTED,
+        }),
+      ],
+    }),
+  },
+};
+
+export const SuccessRecruitApplication: RecruitApplicationPageStory = {
+  name: '수락함',
+  parameters: {
+    ...createMswParameters({
+      recruit: [
+        createMockGetRecruitDetail(mockProjectDetail),
+        createMockGetRecruitApplication({
+          ...mockProjectApplication,
+          matchStatus: MatchStatus.SUCCESS,
+        }),
+      ],
+    }),
+  },
+};
+
+export const InitialRecruitApplication: RecruitApplicationPageStory = {
+  name: '응답 안함',
+  parameters: {
+    ...createMswParameters({
+      recruit: [
+        createMockGetRecruitDetail(mockCompletedProjectDetail),
+        createMockGetRecruitApplication({
+          ...mockProjectApplication,
+          matchStatus: MatchStatus.INITIAL,
+        }),
+      ],
+    }),
+  },
+};
+
+export const Study: RecruitApplicationPageStory = {
+  name: '스터디 리쿠르팅 신청서',
+  parameters: {
+    ...createMswParameters({
+      recruit: [
+        createMockGetRecruitDetail(mockStudyDetail),
+        createMockGetRecruitApplication({
+          ...mockStudyApplication,
+          matchStatus: MatchStatus.PENDING,
+        }),
+      ],
+    }),
   },
 };
