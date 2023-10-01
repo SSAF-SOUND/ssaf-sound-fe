87044aeb9aed2ffccabcd045f8626543ef335f87
Cola <bcad1591@gmail.com>
2023-10-01 23:07:19 +0900

    refactor: 삭제시 toast 텍스트

--- src/components/Recruit/Recruit/RecruitHeader.tsx
+++ src/components/Recruit/Recruit/RecruitHeader.tsx
@@ -1,4 +1,4 @@
-import type { ReportProps } from '~/components/ModalContent';
+import type { UseCommonBottomMenuModalParams } from '~/services/common';
 import type { RecruitDetail } from '~/services/recruit';
 
 import { useRouter } from 'next/router';
@@ -54,14 +54,14 @@ export const RecruitHeader = (props: RecruitHeaderProps) => {
     try {
       await customToast.promise(removeRecruit(), {
         loading: '리쿠르팅을 삭제중입니다.',
-        success: '해당 리쿠르팅을 성공적으로 삭제하였습니다.',
+        success: '리쿠르팅을 성공적으로 삭제하였습니다.',
       });
 
       closeModal();
       await router.replace(routes.recruit.self());
     } catch (err) {}
   };
-  const onClickReport: ReportProps['onClickReport'] = ({
+  const onClickReport: UseCommonBottomMenuModalParams['onClickReport'] = ({
     domain,
     reportReasonId,
   }) => {
