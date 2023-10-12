005a86241baa9cb1e60c7857aebea90383930650
Cola <bcad1591@gmail.com>
2023-10-13 01:27:04 +0900

    refactor: 유저 프로필 페이지 스토리 재작성

--- src/stories/pages/profile/UserProfilePage.stories.tsx
+++ src/stories/pages/profile/UserProfilePage.stories.tsx
@@ -1,41 +1,42 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import type { UserInfo } from '~/services/member';
 
-import { useQueryClient } from '@tanstack/react-query';
-
-import { userInfo } from '~/mocks/handlers/member/data';
+import { createMockGetMyInfo } from '~/mocks/handlers/member/apis/mockGetMyInfo';
+import { createMockGetUserInfo } from '~/mocks/handlers/member/apis/mockGetUserInfo';
+import {
+  mockGetEmptyUserPortfolio,
+  mockGetUserPortfolio,
+} from '~/mocks/handlers/member/apis/mockGetUserPortfolio';
+import { createMockGetUserProfileVisibility } from '~/mocks/handlers/member/apis/mockGetUserProfileVisibility';
+import { mockUserInfo } from '~/mocks/handlers/member/data';
+import {
+  mockGetEmptyJoinedRecruits,
+  mockGetJoinedRecruits,
+} from '~/mocks/handlers/recruit/apis/mockGetJoinedRecruits';
 import ProfilePage from '~/pages/profile/[userId]';
-import { queryKeys } from '~/react-query/common';
-import { useSetMyInfo } from '~/services/member';
 import { PageLayout } from '~/stories/Layout';
+import { createMswParameters } from '~/stories/utils';
 
-const mockMyInfo: UserInfo = {
-  ...userInfo.certifiedSsafyUserInfo,
-  memberId: 1,
-};
-const mockUserInfo: UserInfo = {
-  ...userInfo.certifiedSsafyUserInfo,
-  memberId: 2,
-};
+const myInfo = mockUserInfo.uncertifiedSsafyUserInfo;
+const otherUserInfo = mockUserInfo.certifiedSsafyUserInfo;
+
 const meta: Meta<typeof ProfilePage> = {
-  title: 'Page/Profile/UserProfile',
+  title: 'Page/프로필/다른 사람 프로필',
   component: ProfilePage,
   decorators: [
-    (Story) => {
-      const setMyInfo = useSetMyInfo();
-      setMyInfo(mockMyInfo);
-
-      const queryClient = useQueryClient();
-      queryClient.setQueryData(queryKeys.user.userInfo(NaN), mockUserInfo);
-      return (
-        <PageLayout>
-          <Story />
-        </PageLayout>
-      );
-    },
+    (Story) => (
+      <PageLayout>
+        <Story />
+      </PageLayout>
+    ),
   ],
   parameters: {
     layout: 'fullscreen',
+    ...createMswParameters({
+      common: [
+        createMockGetMyInfo(myInfo),
+        createMockGetUserInfo(otherUserInfo),
+      ],
+    }),
   },
 };
 
@@ -43,4 +44,34 @@ export default meta;
 
 type ProfilePageStory = StoryObj<typeof ProfilePage>;
 
-export const Default: ProfilePageStory = {};
+export const Default: ProfilePageStory = {
+  name: '정상',
+  parameters: {
+    ...createMswParameters({
+      member: [mockGetUserPortfolio, createMockGetUserProfileVisibility(true)],
+      recruit: [mockGetJoinedRecruits],
+    }),
+  },
+};
+
+export const Empty: ProfilePageStory = {
+  name: '빈 데이터',
+  parameters: {
+    ...createMswParameters({
+      member: [
+        mockGetEmptyUserPortfolio,
+        createMockGetUserProfileVisibility(true),
+      ],
+      recruit: [mockGetEmptyJoinedRecruits],
+    }),
+  },
+};
+
+export const IsPrivate: ProfilePageStory = {
+  name: '프로필 비공개',
+  parameters: {
+    ...createMswParameters({
+      member: [mockGetUserPortfolio, createMockGetUserProfileVisibility(false)],
+    }),
+  },
+};
