0a2dc65e568b18712d56c7b8f5ca1b4e0cd8f82f
Cola <bcad1591@gmail.com>
2023-10-12 16:45:16 +0900

    refactor: 내 프로필 스토리 재작성

--- src/stories/pages/profile/MyProfilePage.stories.tsx
+++ src/stories/pages/profile/MyProfilePage.stories.tsx
@@ -1,34 +1,36 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import type { InfiniteData } from '@tanstack/query-core';
-import type { UserPortfolio } from '~/services/member';
-import type { GetJoinedRecruitsApiData } from '~/services/recruit';
 
-import { useQueryClient } from '@tanstack/react-query';
-
-import { userInfo } from '~/mocks/handlers/member/data';
-import MyProfilePage from '~/pages/profile/';
-import { queryKeys } from '~/react-query/common';
-import { useSetMyInfo } from '~/services/member';
-import { RecruitCategoryName } from '~/services/recruit';
+import { createMockGetMyInfo } from '~/mocks/handlers/member/apis/mockGetMyInfo';
+import {
+  mockGetMyEmptyPortfolio,
+  mockGetMyPortfolio,
+} from '~/mocks/handlers/member/apis/mockGetMyPortfolio';
+import { mockUserInfo } from '~/mocks/handlers/member/data';
+import {
+  mockGetEmptyJoinedRecruits,
+  mockGetJoinedRecruits,
+} from '~/mocks/handlers/recruit/apis/mockGetJoinedRecruits';
+import MyProfilePage from '~/pages/profile';
 import { PageLayout } from '~/stories/Layout';
+import { createMswParameters } from '~/stories/utils';
+
+const myInfo = mockUserInfo.certifiedSsafyUserInfo;
 
 const meta: Meta<typeof MyProfilePage> = {
-  title: 'Page/Profile/MyProfile',
+  title: 'Page/프로필/내 프로필',
   component: MyProfilePage,
   decorators: [
-    (Story) => {
-      const setMyInfo = useSetMyInfo();
-      setMyInfo(mockUser);
-
-      return (
-        <PageLayout>
-          <Story />
-        </PageLayout>
-      );
-    },
+    (Story) => (
+      <PageLayout>
+        <Story />
+      </PageLayout>
+    ),
   ],
   parameters: {
     layout: 'fullscreen',
+    ...createMswParameters({
+      common: [createMockGetMyInfo(myInfo)],
+    }),
   },
 };
 
@@ -37,55 +39,21 @@ export default meta;
 type MyProfilePageStory = StoryObj<typeof MyProfilePage>;
 
 export const Default: MyProfilePageStory = {
-  decorators: [
-    (Story) => {
-      const queryClient = useQueryClient();
-      queryClient.removeQueries(myPortfolioQueryKey);
-      queryClient.removeQueries(myJoinedProjectsQueryKey);
-      queryClient.removeQueries(myJoinedStudiesQueryKey);
-      return <Story />;
-    },
-  ],
-};
-
-const mockUser = userInfo.certifiedSsafyUserInfo;
-const myPortfolioQueryKey = queryKeys.user.myPortfolio();
-const myJoinedProjectsQueryKey = queryKeys.recruit.joinedList({
-  category: RecruitCategoryName.PROJECT,
-  memberId: mockUser.memberId,
-});
-const myJoinedStudiesQueryKey = queryKeys.recruit.joinedList({
-  category: RecruitCategoryName.STUDY,
-  memberId: mockUser.memberId,
-});
-
-const emptyPortfolio: UserPortfolio = {
-  memberLinks: [],
-  selfIntroduction: '',
-  skills: [],
-};
-const emptyJoinedRecruits: InfiniteData<GetJoinedRecruitsApiData['data']> = {
-  pages: [
-    {
-      nextCursor: null,
-      isLast: true,
-      recruits: [],
-    },
-  ],
-  pageParams: [null],
+  name: '정상',
+  parameters: {
+    ...createMswParameters({
+      member: [mockGetMyPortfolio],
+      recruit: [mockGetJoinedRecruits],
+    }),
+  },
 };
 
 export const Empty: MyProfilePageStory = {
-  decorators: [
-    (Story) => {
-      const queryClient = useQueryClient();
-      queryClient.setQueryData<UserPortfolio>(
-        myPortfolioQueryKey,
-        emptyPortfolio
-      );
-      queryClient.setQueryData(myJoinedProjectsQueryKey, emptyJoinedRecruits);
-      queryClient.setQueryData(myJoinedStudiesQueryKey, emptyJoinedRecruits);
-      return <Story />;
-    },
-  ],
+  name: '빈 데이터',
+  parameters: {
+    ...createMswParameters({
+      member: [mockGetMyEmptyPortfolio],
+      recruit: [mockGetEmptyJoinedRecruits],
+    }),
+  },
 };
