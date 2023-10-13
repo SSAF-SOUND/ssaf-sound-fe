50cab84d976f4040db9d429e60f5c20599481687
Cola <bcad1591@gmail.com>
2023-10-13 14:53:15 +0900

    chore: mock api typo

--- src/mocks/handlers/member/apis/mockUpdateNickname.ts
+++ src/mocks/handlers/member/apis/mockUpdateNickname.ts
@@ -1,7 +1,9 @@
 import { restError, restSuccess } from '~/mocks/utils';
+import { endpoints } from '~/react-query/common';
+import { API_URL, composeUrls } from '~/utils';
 
 const updateNicknameMethod = 'patch';
-const updateNicknameEndpoint = 'patch';
+const updateNicknameEndpoint = composeUrls(API_URL, endpoints.user.nickname());
 
 export const mockUpdateNickname = restSuccess(
   updateNicknameMethod,
