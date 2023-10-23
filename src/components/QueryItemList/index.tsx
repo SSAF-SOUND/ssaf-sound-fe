db683e779cae2d49e0cf5d2631ebd69846d7dad1
Cola <bcad1591@gmail.com>
2023-10-24 06:16:32 +0900

    feat: QueryItemList 추가

---
+++ src/components/QueryItemList/index.tsx
@@ -0,0 +1,47 @@
+import type { UseQueryResult } from '@tanstack/react-query';
+import type { ReactElement, ReactNode } from 'react';
+
+import { Fragment, memo } from 'react';
+
+import { ErrorMessageWithSsafyIcon } from '~/components/ErrorMessageWithSsafyIcon';
+
+export interface QueryItemListProps<T> {
+  query: UseQueryResult<T>;
+  skeleton: ReactElement;
+  skeletonCount: number;
+  className?: string;
+  render: (data: T) => ReactElement;
+}
+
+export const QueryItemList = <T,>(props: QueryItemListProps<T>) => {
+  const { query, skeleton, skeletonCount, className, render } = props;
+
+  const { isLoading, isError, error, isSuccess, data } = query;
+
+  return (
+    <div className={className}>
+      {isLoading && <Skeletons count={skeletonCount} skeleton={skeleton} />}
+      {isError && <ErrorMessageWithSsafyIcon error={error} />}
+      {isSuccess && render(data)}
+    </div>
+  );
+};
+
+interface SkeletonsProps {
+  count: number;
+  skeleton: ReactElement;
+}
+const Skeletons = memo((props: SkeletonsProps) => {
+  const { count, skeleton } = props;
+  return (
+    <>
+      {Array(count)
+        .fill(undefined)
+        .map((_, index) => (
+          <Fragment key={index}>{skeleton}</Fragment>
+        ))}
+    </>
+  );
+});
+
+Skeletons.displayName = 'Skeletons';
