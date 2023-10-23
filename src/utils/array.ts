6b2b9c3461f868baf909740a49dd519ee15849e8
Cola <bcad1591@gmail.com>
2023-10-23 19:11:44 +0900

    feat: range 유틸리티 함수 추가

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,2 +1,13 @@
 export const concat = <T>(a: T[], b: T[]) => a.concat(b);
 export const add = (a: number, b: number) => a + b;
+
+// [min, min+1, ..., max]
+export const range = (min: number, max: number) => {
+  if (min > max) return [];
+
+  if (min === max) return [min];
+
+  return Array(max - min + 1)
+    .fill(undefined)
+    .map((_, index) => index + min);
+};
