6d746d86fb71376dc7776efc8a9951a3f8500ede
Cola <bcad1591@gmail.com>
2023-10-26 04:36:42 +0900

    feat: escapeHtmlString 추가

---
+++ src/utils/escapeHtmlString.ts
@@ -0,0 +1,8 @@
+export const escapeHtmlString = (dirty: string) => {
+  return dirty
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+};
