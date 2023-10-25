8a5a9bbba02b7f35ab252b733360a7af7312721e
Cola <bcad1591@gmail.com>
2023-10-26 04:39:28 +0900

    feat: content 에 링크 포함시, a태그로 렌더링

--- src/components/ArticleComment/CommentContent.tsx
+++ src/components/ArticleComment/CommentContent.tsx
@@ -1,7 +1,9 @@
 import { css } from '@emotion/react';
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 
 import { fontCss, palettes } from '~/styles/utils';
+import { regex, sanitizeHtml } from '~/utils';
+import { escapeHtmlString } from '~/utils/escapeHtmlString';
 
 interface CommentContentProps {
   content: string;
@@ -11,11 +13,25 @@ interface CommentContentProps {
 
 const CommentContent = memo((props: CommentContentProps) => {
   const { content, modified, className } = props;
+  const htmlString = useMemo(
+    () =>
+      sanitizeHtml(
+        escapeHtmlString(content).replace(
+          regex.looseUrl,
+          (url) => `<a href="${url}">${url}</a>`
+        ),
+        ['a']
+      ),
+    [content]
+  );
 
   return (
     <div css={selfCss} className={className}>
       <p>
-        <span css={contentCss}>{content}</span>{' '}
+        <span
+          css={contentCss}
+          dangerouslySetInnerHTML={{ __html: htmlString }}
+        />{' '}
         {modified && <span css={modifiedCss}>(수정됨)</span>}
       </p>
     </div>
@@ -26,4 +42,7 @@ export default CommentContent;
 
 const selfCss = css(fontCss.style.R14);
 const modifiedCss = css({ color: palettes.primary.default }, fontCss.style.R14);
-const contentCss = css({ whiteSpace: 'pre-wrap' }, fontCss.style.R14);
+const contentCss = css(
+  { whiteSpace: 'pre-wrap', a: { textDecoration: 'underline' } },
+  fontCss.style.R14
+);
