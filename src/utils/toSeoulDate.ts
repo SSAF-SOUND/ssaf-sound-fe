ce87f7388e4e876ff15df8aedeb25b152240dcd0
Cola <bcad1591@gmail.com>
2023-10-28 15:36:46 +0000

    feat: getSeoulDAte -> toSeoulDate

--- src/utils/toSeoulDate.ts
+++ src/utils/toSeoulDate.ts
@@ -5,6 +5,6 @@ import utc from 'dayjs/plugin/utc';
 dayjs.extend(utc);
 dayjs.extend(tz);
 
-export const getSeoulDate = () => {
-  return dayjs().tz('Asia/Seoul').toDate();
+export const toSeoulDate = (date: Date) => {
+  return dayjs(date).tz('Asia/Seoul');
 };
