123f0c9298b26f708b2df92ea346f0ce4856a899
Cola <bcad1591@gmail.com>
2023-11-05 20:45:18 +0900

    feat: 게시글에 수정된 내용이 없을 때 안내메세지 추가

--- src/pages/articles/[articleId]/edit.tsx
+++ src/pages/articles/[articleId]/edit.tsx
@@ -1,7 +1,11 @@
 import type { CustomNextPage } from 'next/types';
 import type { ArticleFormProps } from '~/components/Forms/ArticleForm';
+import type { ArticleFormValues } from '~/components/Forms/ArticleForm/utils';
 
 import { useRouter } from 'next/router';
+
+import { dequal } from 'dequal';
+import { useEffect, useState } from 'react';
 
 import { ArticleError } from '~/components/Article/ArticleError';
 import { FullPageLoader } from '~/components/Common/FullPageLoader';
@@ -14,9 +18,9 @@ import {
   useUpdateArticle,
 } from '~/services/article/hooks';
 import { reconfirmArticleFormUnload } from '~/services/article/utils/reconfirmArticleFormUnload';
+import { handleAxiosError } from '~/utils';
 import { createAuthGuard } from '~/utils/createAuthGuard';
 import { createNoIndexPageMetaData } from '~/utils/createNoIndexPageMetaData';
-import { handleAxiosError } from '~/utils/handleAxiosError';
 import { routes } from '~/utils/routes';
 
 const metaTitle = '게시글 수정';
@@ -38,6 +42,23 @@ const ArticleEditPage: CustomNextPage = () => {
     error: articleCategoriesError,
   } = useArticleCategories();
   const { mutateAsync: updateArticle } = useUpdateArticle(articleId);
+  const [prevArticleFormValuesSnapshot, setPrevArticleFormValuesSnapshot] =
+    useState<ArticleFormValues | undefined>(undefined);
+
+  useEffect(() => {
+    if (!articleDetail) return;
+    if (prevArticleFormValuesSnapshot) return;
+
+    const { title, content, images, boardId, anonymity } = articleDetail;
+
+    setPrevArticleFormValuesSnapshot({
+      title,
+      content,
+      images,
+      category: boardId,
+      anonymous: anonymity,
+    });
+  }, [prevArticleFormValuesSnapshot, articleDetail]);
 
   if (isArticleDetailLoading || isArticleCategoriesLoading) {
     return <FullPageLoader text="데이터를 불러오는 중입니다." />;
@@ -58,9 +79,19 @@ const ArticleEditPage: CustomNextPage = () => {
   const onValidSubmit: ArticleFormProps['onValidSubmit'] = async (
     formValues
   ) => {
+    const redirect = () => router.push(routes.article.detail(articleId));
+    const notChanged = dequal(formValues, prevArticleFormValuesSnapshot);
+
+    if (notChanged) {
+      if (window.confirm('수정된 내용이 없습니다. 게시글 수정을 종료할까요?')) {
+        redirect();
+      }
+      return;
+    }
+
     try {
       await updateArticle(formValues);
-      await router.push(routes.article.detail(articleId));
+      await redirect();
     } catch (err) {
       handleAxiosError(err);
     }
