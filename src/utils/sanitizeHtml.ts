0d54538125a83425b5f4dad3c392d8f732b318d8
Cola <bcad1591@gmail.com>
2023-11-10 18:01:06 +0900

    fix: link addHook 수정
    - isClient 조건문 제거
    - 중복 어트리뷰트 제거 로직 추가

--- src/utils/sanitizeHtml.ts
+++ src/utils/sanitizeHtml.ts
@@ -1,21 +1,23 @@
 import DOMPurify from 'isomorphic-dompurify';
 
-import { isClient } from '~/utils/misc';
+import { deduplicate } from '~/utils/array';
 
 const addHook = () => {
-  if (isClient) {
-    DOMPurify.addHook('afterSanitizeAttributes', function (node) {
-      if (node.nodeName === 'A') {
-        node.setAttribute('target', '_blank');
+  DOMPurify.addHook('afterSanitizeAttributes', function (node) {
+    if (node.nodeName === 'A') {
+      node.setAttribute('target', '_blank');
 
-        const prevRelAttributes = node.getAttribute('rel');
-        const nextRelAttributes = [prevRelAttributes, 'ugc', 'nofollow']
-          .filter(Boolean)
-          .join(' ');
-        node.setAttribute('rel', nextRelAttributes);
-      }
-    });
-  }
+      const prevRelAttributes = node.getAttribute('rel');
+      const nextRelAttributes = deduplicate([
+        prevRelAttributes,
+        'ugc',
+        'nofollow',
+      ])
+        .filter(Boolean)
+        .join(' ');
+      node.setAttribute('rel', nextRelAttributes);
+    }
+  });
 };
 
 addHook();
