6bf699d3560bf510c4c0e94d21024566bde4fcfb
mjsdo <bcad1591@gmail.com>
2023-12-26 10:32:07 +0900

    chore: mock api 추가

---
+++ src/mocks/handlers/notification/apis/mockGetHasNewNotifications.ts
@@ -0,0 +1,25 @@
+import type { GetHasNewNotificationsApiData } from '~/services/notifications';
+
+import { restError, restSuccess } from '~/mocks/utils';
+import { endpoints } from '~/react-query/common';
+import { API_URL, composeUrls } from '~/utils';
+
+const mockGetHasNewNotificationsMethod = 'get';
+const mockGetHasNewNotificationsEndpoint = composeUrls(
+  API_URL,
+  endpoints.user.studentCertification()
+);
+
+export const mockGetHasNewNotifications = restSuccess<
+  GetHasNewNotificationsApiData['data']
+>(mockGetHasNewNotificationsMethod, mockGetHasNewNotificationsEndpoint, {
+  data: { isNew: true },
+});
+
+export const mockGetHasNewNotificationsError = restError(
+  mockGetHasNewNotificationsMethod,
+  mockGetHasNewNotificationsEndpoint,
+  {
+    message: 'mockGetHasNewNotification Error',
+  }
+);
