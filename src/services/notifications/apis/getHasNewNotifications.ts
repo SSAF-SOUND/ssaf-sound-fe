a889c747308c9e03a3d06fea12c5e27de51a0cc4
mjsdo <bcad1591@gmail.com>
2023-12-26 10:32:25 +0900

    chore: type 이름 수정

--- src/services/notifications/apis/getHasNewNotifications.ts
+++ src/services/notifications/apis/getHasNewNotifications.ts
@@ -3,13 +3,13 @@ import type { ApiSuccessResponse } from '~/types';
 import { endpoints } from '~/react-query/common';
 import { privateAxios } from '~/utils';
 
-export type GetHasNewNotificationApiData = ApiSuccessResponse<{
+export type GetHasNewNotificationsApiData = ApiSuccessResponse<{
   isNew: boolean;
 }>;
 
 export const getHasNewNotifications = () => {
   const endpoint = endpoints.notification.hasNew();
-  return privateAxios<GetHasNewNotificationApiData>({
+  return privateAxios<GetHasNewNotificationsApiData>({
     method: 'get',
     url: endpoint,
   }).then((res) => res.data.data.isNew);
