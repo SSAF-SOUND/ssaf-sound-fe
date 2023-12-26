809bd745ef9bb998b01566ee58920f33037c6e02
mjsdo <bcad1591@gmail.com>
2023-12-26 09:10:19 +0900

    chore: props 업데이트

--- src/components/Common/SsafyIcon/LogoCharacter.tsx
+++ src/components/Common/SsafyIcon/LogoCharacter.tsx
@@ -1,19 +1,23 @@
+import type { CSSProperties } from 'react';
+
 import { css } from '@emotion/react';
 import { AccessibleIcon } from '@radix-ui/react-accessible-icon';
 
 import LogoCharacterSVG from '~/assets/images/logo-character.svg';
 import { inlineFlex } from '~/styles/utils';
 
-type LogoCharacterSize = 60;
+type LogoCharacterSize = 60 | number;
 
 interface LogoCharacterProps {
   size?: LogoCharacterSize;
+  className?: string;
+  style?: CSSProperties;
 }
 
 const LogoCharacter = (props: LogoCharacterProps) => {
-  const { size = 60 } = props;
+  const { size = 60, ...restProps } = props;
   return (
-    <div css={selfCss}>
+    <div css={selfCss} {...restProps}>
       <AccessibleIcon label="logo-character">
         <LogoCharacterSVG style={{ height: size }} />
       </AccessibleIcon>
